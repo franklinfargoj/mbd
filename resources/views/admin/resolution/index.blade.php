3e776291d82c393ce96acadbc4d06608a1c96f3c
sushma.patel <sushma.patel@neosofttech.com>
2018-08-24 19:23:27 +0530

    file upload in resolution

--- resources/views/admin/resolution/index.blade.php
+++ resources/views/admin/resolution/index.blade.php
@@ -46,28 +46,81 @@
         </div>
         <div class="portlet-body">
           <div class="table-responsive">
-            <form method="get" action="{{ route('resolution.index') }}">
-              Title<input type="text" name="title" value="{{ isset($getData['title'])?$getData['title']:'' }}"><br><br>
-              Resolution Type
-              <select name="resolution_type_id">
-                <option value="">Select Resolution Type</option>
-                @foreach($resolutionTypes as $resolutionType)
-                  <option value="{{ $resolutionType['id'] }}" {{ (isset($getData['resolution_type_id']) && $getData['resolution_type_id']==$resolutionType['id'])?'selected':'' }}>{{ $resolutionType['name'] }}</option>
-                @endforeach
-              </select>
-              <br><br>
-              Board
-              <select name="board_id">
-                <option value="">Select Board</option>
-                @foreach($boards as $board)
-                  <option value="{{ $board['id'] }}" {{ (isset($getData['board_id']) && $getData['board_id']==$board['id'])?'selected':'' }}>{{ $board['board_name'] }}</option>
-                @endforeach
-              </select>
-              <br><br>
-              From Date<input type="text" name="published_from_date"><br><br>
-              To Date<input type="text" name="published_to_date">
-              <input type="submit" value="search">
-            </form>
+            
+              <div class="portlet-body form">
+                  <form role="form" method="get" action="{{ route('resolution.index') }}">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label class="col-md-4 control-label">
+                Resolution Type
+              </label>
+              <div class="col-md-8">
+                <select name="resolution_type_id" class="form-control">
+                  <option value="">Select Resolution Type</option>
+                  @foreach($resolutionTypes as $resolutionType)
+                    <option value="{{ $resolutionType['id'] }}" {{ (isset($getData['resolution_type_id']) && $getData['resolution_type_id']==$resolutionType['id'])?'selected':'' }}>{{ $resolutionType['name'] }}</option>
+                  @endforeach
+                </select>
+              </div>
+              </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label class="col-md-4 control-label">
+                Board
+              </label>
+              <div class="col-md-8">
+                <select name="board_id" class="form-control">
+                  <option value="">Select Board</option>
+                  @foreach($boards as $board)
+                    <option value="{{ $board['id'] }}" {{ (isset($getData['board_id']) && $getData['board_id']==$board['id'])?'selected':'' }}>{{ $board['board_name'] }}</option>
+                  @endforeach
+                </select>
+              </div>
+                </div>
+              </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="published_from_date" class="col-md-4 control-label">
+                From Date
+              </label>
+              <div class="col-md-8">
+              <input type="date" name="published_from_date" id="published_from_date" class="form-control">
+            </div>
+              </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label class="col-md-4 control-label">
+                To Date
+              </label>
+              <div class="col-md-8">
+                <input type="text" name="published_to_date" class="form-control">
+              </div>
+              </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label class="col-md-4 control-label">
+                  Title
+                </label>
+                <div class="col-md-8">
+                <input type="text" name="title" value="{{ isset($getData['title'])?$getData['title']:'' }}" class="form-control">
+              </div>
+                </div>
+                </div>
+
+                <div class="col-md-6">
+                <input type="submit" value="search" class="btn blue">
+                </div>
+                
+              </form>
+              </div>
+            
             {!! $html->table() !!}
           </div>
         </div>
