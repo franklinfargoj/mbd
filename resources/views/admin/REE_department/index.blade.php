16ff04803570096e66a52ba20ab1636059b78f32
Bhavana Salunkhe <bhavna.salunkhe@neosofttech.com>
2018-09-20 11:39:50 +0530

    chnages made for REE module

--- resources/views/admin/REE_department/index.blade.php
+++ resources/views/admin/REE_department/index.blade.php
@@ -19,19 +19,8 @@
             <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
                 <div class="row align-items-center">
                     <div class="col-xl-8 order-2 order-xl-1">
-                        <form role="form" id="eeForm" method="get" action="{{ route('ee.index') }}">
+                        <form role="form" id="eeForm" method="get" action="{{ route('ree_applications.index') }}">
                             <div class="form-group m-form__group row align-items-center">
-                                <div class="col-md-4">
-                                    <label for="exampleSelect1">Search</label>
-                                    <div class="m-input-icon m-input-icon--left">
-                                        <input type="text" class="form-control m-input m-input--solid" placeholder="Search..." id="m_form_search">
-                                        <span class="m-input-icon__icon m-input-icon__icon--left">
-                                         <span>
-                                         <i class="la la-search"></i>
-                                         </span>
-                                         </span>
-                                    </div>
-                                </div>
                                 <div class="col-md-4">
                                     <div class="form-group m-form__group">
                                         <label for="office_date_from">From Date</label>
