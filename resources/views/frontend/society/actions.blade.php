9cff8f32127d768b33f908aeb443afe308c983a2
Martin Philipose <martin.philipose@wwindia.com>
2018-09-28 23:38:03 +0530

    Changes in society offer letter section.

--- resources/views/frontend/society/actions.blade.php
+++ resources/views/frontend/society/actions.blade.php
@@ -1,5 +1,5 @@
 <a title="View Documents" href="{{ route('documents_uploaded') }}"><i class="icon-pencil"></i>View Documents</a>
-<a title="View Offer letter" href="{{ route('society_offer_letter_download') }}"><i class="icon-pencil"></i>View Offer Letter Application</a>
+<a title="Download Offer Letter Application" href="{{ route('society_offer_letter_application_download') }}" target="_blank" rel="noopener"><i class="icon-pencil"></i>Download Offer Letter Application</a>
 @if($ol_applications->olApplicationStatus[0]->status_id == '3' || $ol_applications->olApplicationStatus[0]->status_id == '4')
 <a title="Edit Documents" href="{{ route('documents_upload') }}"><i class="fa fa-pencil"></i></a>
 @endif
