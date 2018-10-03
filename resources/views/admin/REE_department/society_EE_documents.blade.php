d132415fa5934c2943b920d13584fa75e2fc2677
abhiraj <abhiraj.gaikwad@neosofttech.com>
2018-10-03 15:33:33 +0530

    fixed vp department and cap pages design

--- resources/views/admin/REE_department/society_EE_documents.blade.php
+++ resources/views/admin/REE_department/society_EE_documents.blade.php
@@ -1,58 +1,59 @@
 @extends('admin.layouts.app')
 @section('css')
 <!-- <style> -->
-<link href="{{asset('/frontend/css/dyce_scrutiny.css')}}" rel="stylesheet" type="text/css"/>
+<link href="{{asset('/frontend/css/dyce_scrutiny.css')}}" rel="stylesheet" type="text/css" />
 
 <!-- </style> -->
 @endsection
 @section('content')
 
-<!-- BEGIN: Subheader -->
-<div class="m-subheader ">
-  <div class="d-flex align-items-center">
-     <div class="mr-auto">
-        <h3 class="m-subheader__title m-subheader__title--separator title">
-        DyCE Scrutiny & Remark </h3>
-     </div>
-  </div>
-</div>
-<div class="m-content"></div>
-
-<!-- society and Appointed Architect details -->
- <div class="m-portlet m-portlet--mobile m_panel">
-    <div class="m-portlet__body main_panel">
-      <table class="table mb-0">
-        <thead class="thead-default">
-          <th class="table-data--xs">अ क्र.</th>
-          <th>तपशील</th>
-          <th class="table-data--xs">सोसायटी दस्तावेज</th>
-          <th class="table-data--xs">EE दस्तावेज</th>
-          <th class="table-data--lg">टिप्पणी</th>
-        </thead>
-        <tbody>
-          <?php $i=0; ?>
-          @foreach($societyDocuments[0]->societyDocuments as $data) 
-            <tr>
-              <td>{{$i+1}}</td>
-              <td>{{($data->documents_Name[0]->name)}}</td>
-              <td class="text-center">
-              @if(isset($data->society_document_path))
-                <a href="{{ asset($data->society_document_path) }}">
-                <img class="pdf-icon" src="{{ asset('/img/pdf-icon.svg')}}"></a>
-              @endif
-              </td>
-              <td class="text-center">
-              @if(isset($data->EE_document_path))
-                <a href="{{ asset($data->EE_document_path) }}">
-                <img class="pdf-icon" src="{{ asset('/img/pdf-icon.svg')}}"></a>
-              @endif
-              </td>
-              <td><p class="mb-2">{{($data->comment_by_EE)}}</p></td>
-            </tr>
-            <?php $i++; ?>
-          @endforeach
-        </tbody>
-      </table>
+<div class="col-md-12">
+    <!-- BEGIN: Subheader -->
+    <div class="m-subheader px-0 m-subheader--top">
+        <div class="d-flex align-items-center">
+            <h3 class="m-subheader__title m-subheader__title--separator">
+                DyCE Scrutiny & Remark </h3>
+        </div>
     </div>
-</div> 
-@endsection   
+
+    <!-- society and Appointed Architect details -->
+    <div class="m-portlet m-portlet--mobile m_panel">
+        <div class="m-portlet__body main_panel">
+            <table class="table mb-0">
+                <thead class="thead-default">
+                    <th class="table-data--xs">अ क्र.</th>
+                    <th>तपशील</th>
+                    <th class="table-data--xs">सोसायटी दस्तावेज</th>
+                    <th class="table-data--xs">EE दस्तावेज</th>
+                    <th class="table-data--lg">टिप्पणी</th>
+                </thead>
+                <tbody>
+                    <?php $i=0; ?>
+                    @foreach($societyDocuments[0]->societyDocuments as $data)
+                    <tr>
+                        <td>{{$i+1}}</td>
+                        <td>{{($data->documents_Name[0]->name)}}</td>
+                        <td class="text-center">
+                            @if(isset($data->society_document_path))
+                            <a href="{{ asset($data->society_document_path) }}">
+                                <img class="pdf-icon" src="{{ asset('/img/pdf-icon.svg')}}"></a>
+                            @endif
+                        </td>
+                        <td class="text-center">
+                            @if(isset($data->EE_document_path))
+                            <a href="{{ asset($data->EE_document_path) }}">
+                                <img class="pdf-icon" src="{{ asset('/img/pdf-icon.svg')}}"></a>
+                            @endif
+                        </td>
+                        <td>
+                            <p class="mb-2">{{($data->comment_by_EE)}}</p>
+                        </td>
+                    </tr>
+                    <?php $i++; ?>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/admin/co_department/approve_offer_letter.blade.php
+++ resources/views/admin/co_department/approve_offer_letter.blade.php
@@ -1,76 +1,64 @@
 @extends('admin.layouts.app')
 @section('content')
 
-<div class="custom-wrapper">
-    <div class="col-md-12">
-        <div class="m-portlet__head">
-      <div class="m-subheader ">
+<div class="col-md-12">
+    <div class="m-subheader px-0 m-subheader--top">
         <div class="d-flex align-items-center">
-           <div class="mr-auto">
-              <h3 class="m-subheader__title m-subheader__title--separator title">
-              Approve Offer Letter </h3>
-           </div>
+            <h3 class="m-subheader__title m-subheader__title--separator">
+                Approve Offer Letter </h3>
         </div>
-      </div>
-        </div>
-        <div class="m-grid__item m-grid__item--fluid m-wrapper">
-            <div class="row">
-                <div class="col-md-12">
+    </div>
 
-                        <div class="m-portlet m-portlet--tabs m-portlet--bordered-semi mb-0">
-                            <div class="portlet-body">
-                                <div class="m-portlet__body m-portlet__body--table m-portlet__body--serial-no m-portlet__body--serial-no-pdf">
-                                    <div class="">
-                                        <h3 class="section-title section-title--small">
-                                        View offer letter</h3>
-                                    </div>
-                                        <div class="mt-3 btn-list">
-                                            <button class="btn btn-primary">View</button>
-                                        </div>
-                                    <div class="remarks-suggestions">
-                                        <div class="mt-3">
-                                            <label for="demarkation_comments">Remark by REE</label>
-                                            <textarea id="demarkation_comments" rows="5" cols="30" class="form-control form-control--custom"
-                                                name="demarkation_comments"></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="m-portlet m-portlet--tabs m-portlet--bordered-semi mb-0">
-                            <div class="portlet-body">
-                                <div class="m-portlet__body m-portlet__body--table m-portlet__body--serial-no m-portlet__body--serial-no-pdf">
-                                    <div class="">
-                                        <h3 class="section-title section-title--small">
-                                        Approve Offer letter</h3>
-                                    </div>
-                                    <div class="m-radio-inline">
-                                      <label class="m-radio m-radio--primary">
-                                          <input type="radio" name="remarks-suggestion" class="forward-application" value="1" checked=""> Approve Offer Letter
-                                          <span></span>
-                                      </label>
-                                    </div>
-                                    <div class="remarks-suggestions">
-                                        <div class="mt-3">
-                                            <label for="demarkation_comments">Remark</label>
-                                            <textarea id="demarkation_comments" rows="5" cols="30" class="form-control form-control--custom"
-                                                name="demarkation_comments"></textarea>
-                                        </div>
-                                        <div class="mt-3 btn-list">
-                                            <button class="btn btn-primary">Approve</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>                        
-
+    <div class="m-portlet m-portlet--tabs m-portlet--bordered-semi mb-0">
+        <div class="portlet-body">
+            <div class="m-portlet__body m-portlet__body--table m-portlet__body--serial-no m-portlet__body--serial-no-pdf">
+                <div class="">
+                    <h3 class="section-title section-title--small">
+                        View offer letter</h3>
+                </div>
+                <div class="mt-3 btn-list">
+                    <button class="btn btn-primary">View</button>
+                </div>
+                <div class="remarks-suggestions">
+                    <div class="mt-3">
+                        <label for="demarkation_comments">Remark by REE</label>
+                        <textarea id="demarkation_comments" rows="5" cols="30" class="form-control form-control--custom"
+                            name="demarkation_comments"></textarea>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="m-portlet m-portlet--tabs m-portlet--bordered-semi mb-0">
+        <div class="portlet-body">
+            <div class="m-portlet__body m-portlet__body--table m-portlet__body--serial-no m-portlet__body--serial-no-pdf">
+                <div class="">
+                    <h3 class="section-title section-title--small">
+                        Approve Offer letter</h3>
+                </div>
+                <div class="m-radio-inline">
+                    <label class="m-radio m-radio--primary">
+                        <input type="radio" name="remarks-suggestion" class="forward-application" value="1" checked="">
+                        Approve Offer Letter
+                        <span></span>
+                    </label>
+                </div>
+                <div class="remarks-suggestions">
+                    <div class="mt-3">
+                        <label for="demarkation_comments">Remark</label>
+                        <textarea id="demarkation_comments" rows="5" cols="30" class="form-control form-control--custom"
+                            name="demarkation_comments"></textarea>
+                    </div>
+                    <div class="mt-3 btn-list">
+                        <button class="btn btn-primary">Approve</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
 </div>
 @endsection
-
-
