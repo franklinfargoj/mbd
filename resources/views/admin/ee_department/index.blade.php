ac7744ca1dc5b5eddf79ffda21ca47b5823bb85a
abhiraj <abhiraj.gaikwad@neosofttech.com>
2018-10-06 18:15:42 +0530

    added filter button

--- resources/views/admin/ee_department/index.blade.php
+++ resources/views/admin/ee_department/index.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 
 @if(session()->has('success'))
-    <div class="alert alert-success display_msg">
-        {{ session()->get('success') }}
-    </div>   
+<div class="alert alert-success display_msg">
+    {{ session()->get('success') }}
+</div>
 @endif
 
 <div class="col-md-12">
@@ -12,6 +12,71 @@
     <div class="m-subheader px-0 m-subheader--top">
         <div class="d-flex align-items-center">
             <h3 class="m-subheader__title m-subheader__title--separator">Application for Offer Letter</h3>
+            <button type="button" class="btn btn-transparent ml-auto" data-toggle="collapse" data-target="#filter">
+                <img class="filter-icon" src="{{asset('/img/filter-icon.svg')}}">Filter
+            </button>
+        </div>
+        <div id="filter" class="filter-wrap collapse">
+            <div class="row align-items-center">
+                <div class="col-xl-8 order-2 order-xl-1">
+                    <form role="form" id="eeForm" method="get" action="{{ route('ee.index') }}">
+                        <div class="form-group m-form__group row align-items-center mb-0">
+                            {{--<div class="col-md-4">--}}
+                                {{--<label for="exampleSelect1">Search</label>--}}
+                                {{--<div class="m-input-icon m-input-icon--left">--}}
+                                    {{--<input type="text" class="form-control m-input m-input--solid" placeholder="Search..."
+                                        id="m_form_search">--}}
+                                    {{--<span class="m-input-icon__icon m-input-icon__icon--left">--}}
+                                        {{--<span>--}}
+                                            {{--<i class="la la-search"></i>--}}
+                                            {{--</span>--}}
+                                        {{--</span>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            <div class="col-md-4">
+                                <div class="form-group m-form__group">
+                                    <label for="office_date_from">From Date</label>
+                                    <input type="text" id="office_date_from" name="office_date_from" class="form-control form-control--custom m-input m_datepicker"
+                                        placeholder="From Date" value="{{ isset($getData['office_date_from'])? $getData['office_date_from'] : '' }}">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group m-form__group">
+                                    <label for="office_date_to">To Date</label>
+                                    <input type="text" id="office_date_to" name="office_date_to" class="form-control form-control--custom m-input m_datepicker"
+                                        placeholder="To Date" value="{{ isset($getData['office_date_to'])? $getData['office_date_to'] : '' }}">
+                                </div>
+                            </div>
+
+                            @php
+                            $status = isset($getData['update_status'])? $getData['update_status'] : '';
+                            @endphp
+
+                            <div class="col-md-4">
+                                <div class="form-group m-form__group">
+                                    <label for="office_date_to">Status</label>
+                                    <select class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input"
+                                        id="update_status" name="update_status">
+                                        <option value="">All</option>
+                                        @foreach(config('commanConfig.applicationStatus') as $key =>
+                                        $application_status)
+                                        <option value="{{ $application_status }}"
+                                            {{ ($status == $application_status) ? 'selected' : '' }}>{{
+                                            ucwords(str_replace('_', ' ', $key)) }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-md-8" style="margin-top: 15px;">
+                                <div class="">
+                                    <button type="submit" class="btn btn-primary">Search</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
     <!-- END: Subheader -->
@@ -21,72 +86,20 @@
                 {{--<div class="m-portlet__head-title">--}}
                     {{--<h3 class="m-portlet__head-text">--}}
 
-                    {{--</h3>--}}
+                        {{--</h3>--}}
+                    {{--</div>--}}
                 {{--</div>--}}
+            {{--<a class="btn btn-danger" href="{{route('hearing.create')}}" style="float: right;margin-top: 3%">Add
+                Hearing</a>--}}
             {{--</div>--}}
-            {{--<a class="btn btn-danger" href="{{route('hearing.create')}}" style="float: right;margin-top: 3%">Add Hearing</a>--}}
-        {{--</div>--}}
         <div class="m-portlet__body">
             <!--begin: Search Form -->
             <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                <div class="row align-items-center">
-                    <div class="col-xl-8 order-2 order-xl-1">
-                        <form role="form" id="eeForm" method="get" action="{{ route('ee.index') }}">
-                            <div class="form-group m-form__group row align-items-center">
-                                {{--<div class="col-md-4">--}}
-                                    {{--<label for="exampleSelect1">Search</label>--}}
-                                    {{--<div class="m-input-icon m-input-icon--left">--}}
-                                        {{--<input type="text" class="form-control m-input m-input--solid" placeholder="Search..." id="m_form_search">--}}
-                                        {{--<span class="m-input-icon__icon m-input-icon__icon--left">--}}
-                                         {{--<span>--}}
-                                         {{--<i class="la la-search"></i>--}}
-                                         {{--</span>--}}
-                                         {{--</span>--}}
-                                    {{--</div>--}}
-                                {{--</div>--}}
-                                <div class="col-md-4">
-                                    <div class="form-group m-form__group">
-                                        <label for="office_date_from">From Date</label>
-                                        <input type="text" id="office_date_from" name="office_date_from" class="form-control form-control--custom m-input m_datepicker" placeholder="From Date" value="{{ isset($getData['office_date_from'])? $getData['office_date_from'] : '' }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group m-form__group">
-                                        <label for="office_date_to">To Date</label>
-                                        <input type="text" id="office_date_to" name="office_date_to" class="form-control form-control--custom m-input m_datepicker" placeholder="To Date" value="{{ isset($getData['office_date_to'])? $getData['office_date_to'] : '' }}">
-                                    </div>
-                                </div>
-
-                                @php
-                                    $status = isset($getData['update_status'])? $getData['update_status'] : '';
-                                @endphp
-
-                                <div class="col-md-4">
-                                    <div class="form-group m-form__group">
-                                        <label for="office_date_to">Status</label>
-                                        <select class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input" id="update_status" name="update_status">
-                                            <option value="">All</option>
-                                            @foreach(config('commanConfig.applicationStatus') as $key => $application_status)
-                                                <option value="{{ $application_status }}" {{ ($status == $application_status) ? 'selected' : '' }}>{{ ucwords(str_replace('_', ' ', $key)) }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-8" style="margin-top: 15px;">
-                                    <div class="form-group m-form__group">
-                                        <button type="submit" class="btn btn-primary">Search</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
             </div>
             <!--end: Search Form -->
             <!--begin: Datatable -->
-        {!! $html->table() !!}
-        <!--end: Datatable -->
+            {!! $html->table() !!}
+            <!--end: Datatable -->
         </div>
     </div>
     <input type="hidden" id="myModalBtn" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" />
@@ -99,15 +112,16 @@
 </div>
 @endsection
 @section('datatablejs')
-    {!! $html->scripts() !!}
+{!! $html->scripts() !!}
 
-    <script>
-        $("#update_status").on("change", function () {
-            $("#eeForm").submit();
-        });
+<script>
+    $("#update_status").on("change", function () {
+        $("#eeForm").submit();
+    });
 
-        $(document).ready(function(){
-            $(".display_msg").delay(1000).slideUp(300);
-        });         
-    </script>
+    $(document).ready(function () {
+        $(".display_msg").delay(1000).slideUp(300);
+    });
+
+</script>
 @endsection
--- resources/views/admin/resolution/index.blade.php
+++ resources/views/admin/resolution/index.blade.php
@@ -6,7 +6,78 @@
         <div class="d-flex align-items-center">
             <h3 class="m-subheader__title m-subheader__title--separator">Resolution Listing</h3>
             {{ Breadcrumbs::render('resolution') }}
-            <a class="btn btn-primary ml-auto" href="{{route('resolution.create')}}">Add Resolution</a>
+            <div class="ml-auto">
+                <div class="btn-list">
+                    <button type="button" class="btn btn-transparent ml-auto" data-toggle="collapse" data-target="#filter">
+                        <img class="filter-icon" src="{{asset('/img/filter-icon.svg')}}">Filter
+                    </button>
+                    <button type="submit" name="excel" value="excel" class="btn excel-icon"><img src="{{asset('/img/excel-icon.svg')}}"></button>
+                    <a target="_blank" href="{{route('resolution.print',['published_from_date'=>app('request')->input('published_from_date'),'published_to_date'=>app('request')->input('published_to_date'),'resolution_type_id'=>app('request')->input('resolution_type_id'),'board_id'=>app('request')->input('board_id')])}}"
+                        class="btn print-icon"><img src="{{asset('/img/print-icon.svg')}}"></a>
+                    <a class="btn btn-primary" href="{{route('resolution.create')}}">Add Resolution</a>
+                </div>
+            </div>
+        </div>
+        <div id="filter" class="filter-wrap collapse">
+            <div class="row align-items-center">
+                <div class="col-md-12 order-2 order-xl-1">
+                    <!-- <div class="form-group m-form__group row align-items-center"> -->
+                    <form class="form-group m-form__group row align-items-end mb-0" method="get" action="{{ url('/resolution') }}">
+                        <div class="col-md-3">
+                            <label for="exampleSelect1">Title</label>
+                            <input type="text" class="form-control form-control--custom m-input" placeholder="Search..."
+                                id="m_form_search" name="title" value="{{ (!empty($getData) ? $getData['title'] : '') }}">
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group m-form__group">
+                                <label>From Date</label>
+                                <input type="text" class="form-control form-control--custom m-input m_datepicker"
+                                    placeholder="From Date" name="published_from_date" value="{{ (!empty($getData) ? $getData['published_from_date'] : '') }}">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group m-form__group">
+                                <label>To Date</label>
+                                <input type="text" class="form-control form-control--custom m-input m_datepicker"
+                                    placeholder="From Date" name="published_to_date" value="{{ (!empty($getData) ? $getData['published_to_date'] : '') }}">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group m-form__group">
+                                <label>Resolution Type</label>
+                                <select class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input"
+                                    id="exampleSelect1" name="resolution_type_id">
+                                    <option value="0">Select Resolution Type</option>
+                                    @foreach($resolutionTypes as $resolutionType)
+                                    <option
+                                        {{ (!empty($getData) ? ($getData['resolution_type_id']==$resolutionType['id']?'selected':'') : '') }}
+                                        value="{{ $resolutionType['id'] }}">{{ $resolutionType['name'] }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-3 mt-4">
+                            <div class="form-group m-form__group">
+                                <label>Boards</label>
+                                <select class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input"
+                                    id="exampleSelect1" name="board_id">
+                                    <option value="0">Select Board</option>
+                                    @foreach($boards as $board)
+                                    <option
+                                        {{ (!empty($getData) ? ($getData['board_id']==$board['id']?'selected':'') : '') }}
+                                        value="{{ $board['id'] }}">{{ $board['board_name'] }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn-primary">Search</button>
+                            <button type="submit" name="reset" value="Reset" class="btn btn-primary">Reset</button>
+                        </div>
+                    </form>
+                    <!-- </div> -->
+                </div>
+            </div>
         </div>
     </div>
     <!-- END: Subheader -->
@@ -14,68 +85,7 @@
         <div class="m-portlet__body">
             <!--begin: Search Form -->
             <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                <div class="row align-items-center">
-                    <div class="col-md-12 order-2 order-xl-1">
-                        <!-- <div class="form-group m-form__group row align-items-center"> -->
-                        <form class="form-group m-form__group row align-items-end" method="get" action="{{ url('/resolution') }}">
-                            <div class="col-md-3">
-                                <label for="exampleSelect1">Title</label>
-                                <input type="text" class="form-control form-control--custom m-input" placeholder="Search..."
-                                    id="m_form_search" name="title" value="{{ (!empty($getData) ? $getData['title'] : '') }}">
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group m-form__group">
-                                    <label>From Date</label>
-                                    <input type="text" class="form-control form-control--custom m-input m_datepicker"
-                                        placeholder="From Date" name="published_from_date" value="{{ (!empty($getData) ? $getData['published_from_date'] : '') }}">
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group m-form__group">
-                                    <label>To Date</label>
-                                    <input type="text" class="form-control form-control--custom m-input m_datepicker"
-                                        placeholder="From Date" name="published_to_date" value="{{ (!empty($getData) ? $getData['published_to_date'] : '') }}">
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group m-form__group">
-                                    <label>Resolution Type</label>
-                                    <select class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input"
-                                        id="exampleSelect1" name="resolution_type_id">
-                                        <option value="0">Select Resolution Type</option>
-                                        @foreach($resolutionTypes as $resolutionType)
-                                        <option {{ (!empty($getData) ? ($getData['resolution_type_id']==$resolutionType['id']?'selected':'') : '') }} value="{{ $resolutionType['id'] }}">{{ $resolutionType['name'] }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-3 mt-4">
-                                <div class="form-group m-form__group">
-                                    <label>Boards</label>
-                                    <select class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input"
-                                        id="exampleSelect1" name="board_id">
-                                        <option value="0">Select Board</option>
-                                        @foreach($boards as $board)
-                                        <option {{ (!empty($getData) ? ($getData['board_id']==$board['id']?'selected':'') : '') }} value="{{ $board['id'] }}">{{ $board['board_name'] }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <button type="submit" class="btn btn-primary">Search</button>
-                                <button type="submit" name="reset" value="Reset" class="btn btn-primary">Reset</button>
-                            </div>
-                            <div class="col-md-6 mt-5">
-                                <div class="btn-list text-right">
-                                    <button type="submit" name="excel" value="excel" class="btn excel-icon"><img src="{{asset('/img/excel-icon.svg')}}"></button>
-                                    <a target="_blank" href="{{route('resolution.print',['published_from_date'=>app('request')->input('published_from_date'),'published_to_date'=>app('request')->input('published_to_date'),'resolution_type_id'=>app('request')->input('resolution_type_id'),'board_id'=>app('request')->input('board_id')])}}"
-                                        class="btn print-icon"><img src="{{asset('/img/print-icon.svg')}}"></a>
-                                </div>
-                            </div>
-                        </form>
-                        <!-- </div> -->
-                    </div>
-                </div>
+
             </div>
             <!--end: Search Form -->
             <!--begin: Datatable -->
