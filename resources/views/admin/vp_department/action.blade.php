496b49e881243b67798a85270571fb6db22f5b99
Bhavana Salunkhe <bhavna.salunkhe@neosofttech.com>
2018-10-07 16:34:49 +0530

    add action

--- resources/views/admin/vp_department/action.blade.php
+++ resources/views/admin/vp_department/action.blade.php
@@ -1,3 +1,20 @@
+<li class="m-menu__item m-menu__item--active m-menu__item--submenu" id="sub-menu" aria-haspopup="true"
+    m-menu-submenu-toggle="hover">
+    <a href="javascript:void(0);" class="m-menu__link m-menu__toggle">
+        <i class="m-menu__link-icon flaticon-line-graph"></i>
+        <span class="m-menu__link-title">
+            <span class="m-menu__link-wrap">
+                <span class="m-menu__link-text">
+                    Actions
+                </span>
+                <i class="m-menu__ver-arrow la la-angle-right"></i>
+            </span>
+        </span>
+    </a>
+    <div class="m-menu__submenu" m-hidden-height="160" style=""><span class="m-menu__arrow"></span>
+        <ul class="m-menu__subnav">
+
+
 
 <li class="m-menu__item m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover"><a class="m-menu__link m-menu__toggle" title="view_Application" href="{{ route('vp.view_application', $ol_application->id) }}"><img class="radio-icon" src="{{ asset('/img/radio-icon.svg')}}"><span class="m-menu__link-text">View Applications</span></a></li>
 
@@ -33,4 +50,7 @@
 		<img class="radio-icon" src="{{ asset('/img/radio-icon.svg')}}"><span class="m-menu__link-text">CAP Notes</span>
 	</a>
 </li>
-
+      
+    </ul>
+</div>
+</li>
