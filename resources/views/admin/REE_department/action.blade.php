7847b3f8fcfe60e502dc57a7d38b66a397fb31cc
Martin Philipose <martin.philipose@wwindia.com>
2018-10-11 01:36:09 +0530

    Changes in route name for active class in ree.

--- resources/views/admin/REE_department/action.blade.php
+++ resources/views/admin/REE_department/action.blade.php
@@ -40,7 +40,7 @@ $route=\Request::route()->getName();
             </a>
         </li>
 
-        <li class="m-menu__item m-menu__item--submenu {{($route=='cap.EE_scrutiny_remark')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu {{($route=='ree.EE_Scrutiny_Remark')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" title="EE Scrutiny & Remarks" href="{{route('ree.EE_Scrutiny_Remark',$ol_application->id)}}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
