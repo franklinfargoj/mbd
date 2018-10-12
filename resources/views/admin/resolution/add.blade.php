17cbd52378acc53ebdd78d847686d8b508b0209c
abhiraj <abhiraj.gaikwad@neosofttech.com>
2018-10-12 16:10:03 +0530

    fixed add page design on resolutions page

--- resources/views/admin/resolution/add.blade.php
+++ resources/views/admin/resolution/add.blade.php
@@ -9,13 +9,13 @@
         </div>
     </div>
     <!-- END: Subheader -->
-    <div class="m-portlet m-portlet--mobile">
+    <div class="m-portlet m-portlet--mobile m-portlet--forms-view">
         <form id="add_resolutionForm" role="form" method="post" class="m-form m-form--rows m-form--label-align-right"
             action="{{route('resolution.store')}}" enctype="multipart/form-data">
             @csrf
             <div class="m-portlet__body m-portlet__body--spaced">
                 <div class="form-group m-form__group row">
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 form-group">
                         <div class="@if($errors->has('board')) has-error @endif">
                             <label class="col-form-label">Board:</label>
                             <span class="star">*</span>
@@ -30,7 +30,7 @@
                         </div>
                     </div>
 
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 offset-sm-1 form-group">
                         <div class="@if($errors->has('department')) has-error @endif">
                             <label class="col-form-label">Department:</label><span class="star">*</span>
                             <select name="department" id="department_id" class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input">
@@ -43,7 +43,7 @@
                 </div>
 
                 <div class="form-group m-form__group row">
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 form-group">
                         <div class="@if($errors->has('resolution_type')) has-error @endif">
                             <label class="col-form-label">Resolution Type:</label><span class="star">*</span>
                             <select name="resolution_type" id="resolution_type_id" class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input">
@@ -56,7 +56,7 @@
                         </div>
                     </div>
 
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 offset-sm-1 form-group">
                         <div class="@if($errors->has('resolution_code')) has-error @endif">
                             <label class="col-form-label">Resolution Code:</label><span class="star">*</span>
                             <input type="text" name="resolution_code" id="resolution_code" class="form-control form-control--custom m-input"
@@ -67,7 +67,7 @@
                 </div>
 
                 <div class="form-group m-form__group row">
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 form-group">
                         <label class="col-form-label">Title:</label><span class="star">*</span>
                         <div class="@if($errors->has('title')) has-error @endif">
                             <input type="text" name="title" id="title" class="form-control form-control--custom m-input"
@@ -75,7 +75,7 @@
                             <span class="help-block text-danger">{{$errors->first('title')}}</span>
                         </div>
                     </div>
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 offset-sm-1 form-group">
                         <div class="@if($errors->has('revision_log_message')) has-error @endif">
                             <label class="col-form-label">Keyword:</label>
                             <textarea name="keyword" id="keyword" class="form-control form-control--custom form-control--fixed-height m-input"></textarea>
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="form-group m-form__group row">
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 form-group">
                        <label class="col-form-label">Description:</label> <span class="star">*</span>
                         <div class="@if($errors->has('description')) has-error @endif">
                             <textarea name="description" id="description" class="form-control form-control--custom form-control--fixed-height m-input">{{old('description')}}</textarea>
@@ -93,7 +93,7 @@
                         </div>
                     </div>
 
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 offset-sm-1 form-group">
                         <label class="col-form-label">Attach File:</label><span class="star">*</span>
                         <div class="@if($errors->has('file')) has-error @endif">
                             <div class="custom-file">
@@ -107,7 +107,7 @@
 
                 <div class="form-group m-form__group row">
 
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 form-group">
                         <label class="col-form-label">Language:</label><span class="star">*</span>
                         <div class="@if($errors->has('language')) has-error @endif">
                             <input type="text" name="language" id="language" class="form-control form-control--custom m-input"
@@ -116,7 +116,7 @@
                         </div>
                     </div>
 
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 offset-sm-1 form-group">
                         <div class="@if($errors->has('reference_link')) has-error @endif">
                             <label class="col-form-label">Reference Link (if any):</label><span class="star">*</span>
                             <input type="text" name="reference_link" id="reference_link" class="form-control form-control--custom m-input"
@@ -127,7 +127,7 @@
                 </div>
 
                 <div class="form-group m-form__group row">
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 form-group">
                         <div class="@if($errors->has('published_date')) has-error @endif">
                             <label class="col-form-label">Published Date:</label><span class="star">*</span>
                             <input type="text" name="published_date" id="published_date" class="form-control form-control--custom m-input m_datepicker"
@@ -135,7 +135,7 @@
                             <span class="help-block text-danger">{{$errors->first('published_date')}}</span>
                         </div>
                     </div>
-                    <div class="col-lg-6 form-group">
+                    <div class="col-sm-4 offset-sm-1 form-group">
                         <div class="@if($errors->has('revision_log_message')) has-error @endif">
                            <label class="col-form-label">Revision Log Message:</label> <span class="star">*</span>
                             <textarea name="revision_log_message" id="revision_log_message" class="form-control form-control--custom form-control--fixed-height m-input">{{old('revision_log_message')}}</textarea>
@@ -149,7 +149,7 @@
             <div class="m-portlet__foot m-portlet__no-border m-portlet__foot--fit">
                 <div class="m-form__actions px-0">
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-sm-4">
                             <div class="btn-list">
                                 <button type="submit" class="btn btn-primary">Save</button>
                                 <a href="{{url('/resolution')}}" class="btn btn-secondary">Cancel</a>
