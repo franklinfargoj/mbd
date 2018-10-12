dce50033252de557c03d5639979ed63abadfc51a
abhiraj <abhiraj.gaikwad@neosofttech.com>
2018-10-12 15:50:06 +0530

    minor changes

--- resources/views/admin/society_detail/index.blade.php
+++ resources/views/admin/society_detail/index.blade.php
@@ -7,7 +7,8 @@
             <h3 class="m-subheader__title m-subheader__title--separator">Society Details</h3>
             {{ Breadcrumbs::render('society_detail') }}
             <div class="btn-list text-right ml-auto">
-                <a href="{{route('society_detail.index',['excel'=>'excel'])}}" name="excel" value="excel" class="btn excel-icon"><img src="{{asset('/img/excel-icon.svg')}}"></a>
+                <a href="{{route('society_detail.index',['excel'=>'excel'])}}" name="excel" value="excel" class="btn excel-icon"><img
+                        src="{{asset('/img/excel-icon.svg')}}"></a>
                 <a target="_blank" href="{{route('society_detail.print')}}" class="btn print-icon"><img src="{{asset('/img/print-icon.svg')}}"></a>
             </div>
         </div>
@@ -21,64 +22,61 @@
             </button> {{ Session::get('success') }}
         </div>
         @endif
-        <div class="m-portlet__body data-table--custom data-table--icons data-table--actions mb-3">
-                <!-- <a href="{{ url('/village_detail') }}" class="btn btn-link"><i class="fa fa-long-arrow-left" style="padding-right: 8px;"></i>Back</a> -->
-            </div>
-            <!--begin: Search Form -->
-            {{--<div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                <div class="container-fluid">
-                    <div class="row align-items-center">
-                        <div class="col-xl-8 order-2 order-xl-1">
-                            <div class="form-group m-form__group row align-items-center">
-                                <div class="col-md-4">
-                                    <label for="exampleSelect1">Search</label>
-                                    <div class="m-input-icon m-input-icon--left">
-                                        <input type="text" class="form-control m-input m-input--solid" placeholder="Search..."
-                                            id="m_form_search">
-                                        <span class="m-input-icon__icon m-input-icon__icon--left">
-                                            <span><i class="la la-search"></i></span>
-                                        </span>
-                                    </div>
+        <!--begin: Search Form -->
+        {{--<div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-xl-8 order-2 order-xl-1">
+                        <div class="form-group m-form__group row align-items-center">
+                            <div class="col-md-4">
+                                <label for="exampleSelect1">Search</label>
+                                <div class="m-input-icon m-input-icon--left">
+                                    <input type="text" class="form-control m-input m-input--solid" placeholder="Search..."
+                                        id="m_form_search">
+                                    <span class="m-input-icon__icon m-input-icon__icon--left">
+                                        <span><i class="la la-search"></i></span>
+                                    </span>
                                 </div>
-                                <div class="col-md-4">
-                                    <div class="form-group m-form__group">
-                                        <label>Resolution Type</label>
-                                        <select class="form-control m-input m-input--square" id="exampleSelect1">
-                                            <option>Mhada resolutions</option>
-                                            <option>MBR Resolutions</option>
-                                        </select>
-                                    </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group m-form__group">
+                                    <label>Resolution Type</label>
+                                    <select class="form-control m-input m-input--square" id="exampleSelect1">
+                                        <option>Mhada resolutions</option>
+                                        <option>MBR Resolutions</option>
+                                    </select>
                                 </div>
-                                <div class="col-md-4">
-                                    <div class="form-group m-form__group">
-                                        <label>From Date</label>
-                                        <input type="date" class="form-control m-input m-input--solid" placeholder="From Date">
-                                    </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group m-form__group">
+                                    <label>From Date</label>
+                                    <input type="date" class="form-control m-input m-input--solid" placeholder="From Date">
                                 </div>
-                                <div class="col-md-4">
-                                    <div class="form-group m-form__group">
-                                        <label>To Date</label>
-                                        <input type="date" class="form-control m-input m-input--solid" placeholder="From Date">
-                                    </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group m-form__group">
+                                    <label>To Date</label>
+                                    <input type="date" class="form-control m-input m-input--solid" placeholder="From Date">
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>--}}
-            <!--end: Search Form -->
-            <!--begin: Datatable -->
-            {!! $html->table() !!}
-            <!--end: Datatable -->
-        </div>
+            </div>
+        </div>--}}
+        <!--end: Search Form -->
+        <!--begin: Datatable -->
+        {!! $html->table() !!}
+        <!--end: Datatable -->
     </div>
-    <input type="hidden" id="myModalBtn" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" />
+</div>
+<input type="hidden" id="myModalBtn" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" />
 
-    <!-- Modal -->
-    <div id="myModal" class="modal fade" role="dialog">
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
 
-    </div>
-    <!-- END EXAMPLE TABLE PORTLET-->
+</div>
+<!-- END EXAMPLE TABLE PORTLET-->
 </div>
 @endsection
 <?php //dd($html->scripts()); ?>
