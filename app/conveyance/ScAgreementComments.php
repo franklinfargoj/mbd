f00cd728a3c6344e40381ca11bd6a8465d059f32
Prajakta <prajusisale777@gmail.com>
2018-10-16 18:31:19 +0530

    added models

---
+++ app/conveyance/ScAgreementComments.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\conveyance;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ScAgreementComments extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'sc_agreement_comments';
+
+    /**
+     * The database primary key value.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'application_id',
+        'user_id',
+        'role_id',
+        'agreement_type_id',
+        'remark',
+    ];
+
+    /**
+     * Indicates if the model has update and creation timestamps.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+}
