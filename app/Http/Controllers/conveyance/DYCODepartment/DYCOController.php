b24ee0ed69d631468b2b91ed0562d3b878932ee8
Bhavana Salunkhe <bhavna.salunkhe@neosofttech.com>
2018-10-16 15:30:09 +0530

    working on DYCO dept for conveyance

--- app/Http/Controllers/conveyance/DYCODepartment/DYCOController.php
+++ app/Http/Controllers/conveyance/DYCODepartment/DYCOController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\conveyance\DYCODepartment;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\conveyance\conveyanceCommonController;
+use App\conveyance\ConveyanceChecklistScrutiny;
 use Config;
 use Yajra\DataTables\DataTables;
 
@@ -16,62 +18,56 @@ class DYCOController extends Controller
     }	
 	public function index(Request $request, Datatables $datatables){
 
-        $data = $this->common->
+        $data = $this->common->listApplicationData($request);
 
         $columns = [
             ['data' => 'radio','name' => 'radio','title' => '','searchable' => false],
             ['data' => 'rownum','name' => 'rownum','title' => 'Sr No.','searchable' => false],
             ['data' => 'application_no','name' => 'application_no','title' => 'Application Number'],
             ['data' => 'date','name' => 'date','title' => 'Date', 'class' => 'datatable-date'],
-            ['data' => 'eeApplicationSociety.name','name' => 'eeApplicationSociety.name','title' => 'Society Name'],
-            ['data' => 'eeApplicationSociety.building_no','name' => 'eeApplicationSociety.building_no','title' => 'building No'],
-            ['data' => 'eeApplicationSociety.address','name' => 'eeApplicationSociety.address','title' => 'Address', 'class' => 'datatable-address'],
-            // ['data' => 'model','name' => 'model','title' => 'Model'],
+            ['data' => 'societyApplication.name','name' => 'societyApplication.name','title' => 'Society Name'],
+            ['data' => 'societyApplication.building_no','name' => 'societyApplication.building_no','title' => 'building No'],
+            ['data' => 'societyApplication.address','name' => 'societyApplication.address','title' => 'Address', 'class' => 'datatable-address'],
              ['data' => 'Status','name' => 'Status','title' => 'Status'],
-            // ['data' => 'actions','name' => 'actions','title' => 'Actions','searchable' => false,'orderable'=>false],
         ];
 
         if ($datatables->getRequest()->ajax()) {
-
-
-            return $datatables->of($dyce_application_data)
-                ->editColumn('rownum', function () {
+            return $datatables->of($data)
+                ->editColumn('rownum', function ($data) {
                     static $i = 0; $i++; return $i;
                 })
-                ->editColumn('radio', function () {
-                    // $url = route('dyce.view_application', $dyce_application_data->id);
-                    return '<label class="m-radio m-radio--primary m-radio--link"><input type="radio" name="village_data_id"><span></span></label>';
+                ->editColumn('radio', function ($data) {
+                    $url = route('dyco.conveyance_application', $data->id);
+                    return '<label class="m-radio m-radio--primary m-radio--link"><input type="radio" name="application_id" onclick="geturl(this.value);" value="'.$url.'" ><span></span></label>';
+                })                              
+                ->editColumn('societyApplication.name', function ($data) {
+                    return $data->societyApplication->name;
+                })
+                ->editColumn('societyApplication.building_no', function ($data) {
+                    return $data->societyApplication->building_no;
+                })
+                ->editColumn('societyApplication.address', function ($data) {
+                    return '<span>'.$data->societyApplication->address.'</span>';
                 })                
-                ->editColumn('eeApplicationSociety.name', function () {
-                    // return $dyce_application_data->eeApplicationSociety->name;
+                ->editColumn('date', function ($data) {
+                    return date(config('commanConfig.dateFormat'), strtotime($data->created_at));
                 })
-                ->editColumn('eeApplicationSociety.building_no', function () {
-                    // return $dyce_application_data->eeApplicationSociety->building_no;
-                })
-                ->editColumn('eeApplicationSociety.address', function () {
-                    // return "<span>".$dyce_application_data->eeApplicationSociety->address."</span>";
-                })                
-                ->editColumn('date', function () {
-                    // return date(config('commanConfig.dateFormat'), strtotime($dyce_application_data->submitted_at));
-                })
-                // ->editColumn('actions', function ($dyce_application_data) use($request){
-                //    return view('admin.DYCE_department.action', compact('dyce_application_data','request'))->render();
-                // })
-                ->editColumn('Status', function () {
-                    // $status = $listArray->olApplicationStatusForLoginListing[0]->status_id;
 
-                    // if($request->update_status)
-                    // {
-                    //     if($request->update_status == $status){
-                    //         $config_array = array_flip(config('commanConfig.applicationStatus'));
-                    //         $value = ucwords(str_replace('_', ' ', $config_array[$status]));
-                    //         return '<span class="m-badge m-badge--'. config('commanConfig.applicationStatusColor.'.$status) .' m-badge--wide">'.$value.'</span>';
-                    //     }
-                    // }else{
-                    //     $config_array = array_flip(config('commanConfig.applicationStatus'));
-                    //     $value = ucwords(str_replace('_', ' ', $config_array[$status]));
-                    //     return '<span class="m-badge m-badge--'. config('commanConfig.applicationStatusColor.'.$status) .' m-badge--wide">'.$value.'</span>';
-                    // }
+                ->editColumn('Status', function ($data) use ($request) {
+                    $status = $data->scApplicationLog->status_id;
+
+                    if($request->update_status)
+                    {
+                        if($request->update_status == $status){
+                            $config_array = array_flip(config('commanConfig.applicationStatus'));
+                            $value = ucwords(str_replace('_', ' ', $config_array[$status]));
+                            return '<span class="m-badge m-badge--'. config('commanConfig.applicationStatusColor.'.$status) .' m-badge--wide">'.$value.'</span>';
+                        }
+                    }else{
+                        $config_array = array_flip(config('commanConfig.applicationStatus'));
+                        $value = ucwords(str_replace('_', ' ', $config_array[$status]));
+                        return '<span class="m-badge m-badge--'. config('commanConfig.applicationStatusColor.'.$status) .' m-badge--wide">'.$value.'</span>';
+                    }
 
                 })
                 ->rawColumns(['radio','society_name', 'Status', 'building_name', 'society_address','date','eeApplicationSociety.address'])
@@ -91,9 +87,33 @@ class DYCOController extends Controller
             "order"      => [1, "asc" ],
             "pageLength" => $this->list_num_of_records_per_page
         ];
+    }
+
+    public function ViewApplication(Request $request,$applicationId){
+        
+        $data = $this->common->listApplicationData($request);
+        $data->folder = 'dyco_department';
+        $data->id = $applicationId;
+        return view('admin.conveyance.dyco_department.view_application',compact('data'));
     } 
 
-    public function showChecklist(Request $request){
-    	return view('admin.conveyance.dyco_department.checklist_office_note');
+    //display checklist and office note page
+    public function showChecklist(Request $request,$applicationId){
+
+        $data = $this->common->listApplicationData($request);
+        $data->id = $applicationId;
+    	return view('admin.conveyance.dyco_department.checklist_office_note',compact('data'));
+    }
+
+    // save/update checklist data
+    public function storeChecklistData(Request $request){
+        $applicationId = $request->applicationId;
+        $arrData = $request->all();
+        unset($arrData['_token']);
+
+        $checklistData = ConveyanceChecklistScrutiny::where('application_id',$applicationId)->updateOrCreate($arrData);
+
+        dd($checklistData);
+
     }
 }
