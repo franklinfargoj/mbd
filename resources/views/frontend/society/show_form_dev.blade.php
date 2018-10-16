6a2cb47501ab61fcc7bab9c3193df0bfcd4d8dc2
Martin Philipose <martin.philipose@wwindia.com>
2018-10-16 14:46:27 +0530

    Added form and preview page society offer letter.

---
+++ resources/views/frontend/society/show_form_dev.blade.php
@@ -0,0 +1,97 @@
+@extends('admin.layouts.app')
+@section('content')
+    <div class="col-md-12">
+        <div class="m-subheader px-0 m-subheader--top">
+            <div class="d-flex align-items-center">
+                <h3 class="m-subheader__title m-subheader__title--separator">Redevelopment Through Developer</h3>
+                {{ Breadcrumbs::render('Hearing Create') }}
+
+            </div>
+        </div>
+        <!-- END: Subheader -->
+        <div class="m-portlet m-portlet--mobile m-portlet--forms-view">
+
+            <form id="save_offer_letter_application_dev" role="form" method="post" class="m-form m-form--rows m-form--label-align-right" action="{{ route('save_offer_letter_application_dev') }}">
+                @csrf
+                <div class="m-portlet__body m-portlet__body--spaced">
+                    <div class="form-group m-form__group row">
+                        <div class="col-sm-4 form-group">
+                            <label class="col-form-label" for="application_type_id">Application Type:</label>
+                            <select class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input" id="layouts" name="layout_id" required>
+                                @foreach($layouts as $layout)
+                                    <option value="{{ $layout['id'] }}">{{ $layout['layout_name'] }}</option>
+                                @endforeach
+                            </select>
+                            <span class="help-block">{{$errors->first('application_type_id')}}</span>
+                        </div>
+                    </div>
+                    <div class="form-group m-form__group row">
+                        <div class="col-sm-4 form-group">
+                            <label class="col-form-label" for="preceding_officer_name">Department:</label>
+                            <input type="text" id="department_name" name="department_name" class="form-control form-control--custom m-input" value="EE" readonly>
+                            <input type="hidden" name="application_master_id" value="{{ $id }}">
+                            <span class="help-block">{{$errors->first('department_name')}}</span>
+                        </div>
+                        <div class="col-sm-4 offset-sm-1 form-group">
+                            <label class="col-form-label" for="case_year">Building No:</label>
+                            <input type="text" id="building_no" name="building_no" class="form-control form-control--custom m-input" value="{{ $society_details->building_no }}">
+                            <span class="help-block">{{$errors->first('building_no')}}</span>
+                        </div>
+                    </div>
+
+                    <div class="form-group m-form__group row">
+                        <div class="col-sm-4 form-group">
+                            <label class="col-form-label" for="name">Society Name:</label>
+                            <input type="text" id="name" name="name" class="form-control form-control--custom m-input" value="{{ $society_details->name }}">
+                            <span class="help-block">{{$errors->first('name')}}</span>
+                        </div>
+                        <div class="col-sm-4 offset-sm-1 form-group">
+                            <label class="col-form-label" for="address">Society Address:</label>
+                            <textarea id="address" name="address" class="form-control form-control--custom form-control--fixed-height m-input">{{ $society_details->address }}</textarea>
+                            <span class="help-block">{{$errors->first('address')}}</span>
+                        </div>
+                    </div>
+
+                    <div class="form-group m-form__group row">
+                        <div class="col-sm-4 form-group">
+                            <label class="col-form-label" for="date_of_meeting">Date of meeting:</label>
+                            <input type="text" id="m_datepicker" name="date_of_meeting" class="form-control form-control--custom m-input m_datepicker" value="{{ old('date_of_meeting') }}">
+                            <span class="help-block">{{$errors->first('date_of_meeting')}}</span>
+                        </div>
+                        <div class="col-sm-4 offset-sm-1 form-group">
+                            <label class="col-form-label" for="resolution_no">Resolution No:</label>
+                            <input type="text" id="resolution_no" name="resolution_no" class="form-control form-control--custom form-control--fixed-height m-input" value="{{ old('resolution_no') }}">
+                            <span class="help-block">{{$errors->first('resolution_no')}}</span>
+                        </div>
+                    </div>
+
+                    <div class="form-group m-form__group row">
+                        <div class="col-sm-4 form-group">
+                            <label class="col-form-label" for="architect_name">Architect Name:</label>
+                            <input type="text" id="architect_name" name="architect_name" class="form-control form-control--custom m-input" value="{{ old('architect_name') }}">
+                            <span class="help-block">{{$errors->first('architect_name')}}</span>
+                        </div>
+                        <div class="col-sm-4 offset-sm-1 form-group">
+                            <label class="col-form-label" for="developer_name">Developer Name:</label>
+                            <input type="text" id="developer_name" name="developer_name" class="form-control form-control--custom form-control--fixed-height m-input" value="{{ old('developer_name') }}">
+                            <span class="help-block">{{$errors->first('developer_name')}}</span>
+                        </div>
+                    </div>
+
+                    <div class="m-portlet__foot m-portlet__no-border m-portlet__foot--fit">
+                        <div class="m-form__actions px-0">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <div class="btn-list">
+                                        <button type="submit"  class="btn btn-primary">Save</button>
+                                        <a href="{{url('/hearing')}}" class="btn btn-secondary">Cancel</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
