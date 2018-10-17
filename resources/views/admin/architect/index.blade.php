41e02c891ccfce1a42f2bd7fcd537472bf341bec
abhiraj <abhiraj.gaikwad@neosofttech.com>
2018-10-17 19:39:13 +0530

    fixed index page for architect application

--- resources/views/admin/architect/index.blade.php
+++ resources/views/admin/architect/index.blade.php
@@ -1,40 +1,4 @@
 @extends('admin.layouts.app')
-@section('css')
-<style>
-  /* Add a black background color to the top navigation */
-.topnav {
-    background-color: #E5731A;
-    overflow: hidden;
-}
-
-/* Style the links inside the navigation bar */
-.topnav a {
-  float: left;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 13px;
-}
-
-/* Change the color of links on hover */
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-/* Add a color to the active/current link */
-.topnav a.active {
-  background-color: white;
-  color: black;
-}
-
-/* Right-aligned section inside the top navigation */
-.topnav-right {
-  float: right;
-}
-  </style>
-@endsection
 @section('content')
 <div class="col-md-12">
     <!-- BEGIN: Subheader -->
@@ -43,104 +7,95 @@
             <h3 class="m-subheader__title m-subheader__title--separator">Architect Applications
         </div>
         </h3>
-            @if(session('error'))
-            <div class="alert alert-danger">{{ session('error') }}</div>
-            @endif
-            @if(session('success'))
-            <div class="alert alert-success">{{ session('success') }}</div>
-            @endif
+        @if(session('error'))
+        <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
+        @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
     </div>
     <!-- END: Subheader -->
-    <div class="m-portlet m-portlet--mobile">
-        <div class="m-portlet__body">
-            <!--begin: Search Form -->
-            <div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
-                <div class="row align-items-center">
-                    <div class="col-md-12 order-2 order-xl-1">
-                        <!-- <div class="form-group m-form__group row align-items-center"> -->
-                        <form class="form-group m-form__group row align-items-end" method="get" action="{{url('architect_application')}}">
-                            <div class="col-md-3">
-                                <label for="exampleSelect1">Search For</label>
-                                <input type="text" class="form-control form-control--custom m-input" placeholder="Application No, Candidate Name, Email ID OR Mobile No" title="Enter Application No, Candidate Name, Email ID OR Mobile No"
-                                    id="m_form_search" name="keyword" value="{{ (!empty($getData) ? (isset($getData['keyword'])?$getData['keyword']:'') : '') }}">
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group m-form__group">
-                                    <label>From Date</label>
-                                    <input type="text" class="form-control form-control--custom m-input m_datepicker"
-                                        placeholder="From Date" name="from" value="{{ (!empty($getData) ? (isset($getData['from'])?$getData['from']:'') : '') }}">
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group m-form__group">
-                                    <label>To Date</label>
-                                    <input type="text" class="form-control form-control--custom m-input m_datepicker"
-                                        placeholder="From Date" name="to" value="{{ (!empty($getData) ? (isset($getData['ro'])?$getData['to']:'') : '') }}">
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group m-form__group">
-                                    <label>Sort by Status</label>
-                                    <select class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input"
-                                        id="exampleSelect1" name="status">
-                                        <option value="">All</option>
-                                        @foreach(config('commanConfig.architect_applicationStatus') as $key=>$value)
-                                        <option {{ (!empty($getData) ? (isset($getData['status'])?($getData['status']==$value?'selected':''):'') : '') }} value="{{$value}}">{{$key}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <button type="submit" class="btn btn-primary">Search</button>
-                                <button type="submit" name="reset" value="Reset" class="btn btn-primary">Reset</button>
-                            </div>
-                            <!-- <div class="col-md-6 mt-5">
+    <div class="m-portlet m-portlet--compact filter-wrap">
+        <div class="row align-items-center row--filter">
+            <div class="col-md-12">
+                <!-- <div class="form-group m-form__group row align-items-center"> -->
+                <form class="form-group m-form__group row align-items-end" method="get" action="{{url('architect_application')}}">
+                    <div class="col-md-2">
+                        <input type="text" class="form-control form-control--custom m-input" placeholder="Application No, Candidate Name, Email ID OR Mobile No"
+                            title="Enter Application No, Candidate Name, Email ID OR Mobile No" id="m_form_search" name="keyword"
+                            value="{{ (!empty($getData) ? (isset($getData['keyword'])?$getData['keyword']:'') : '') }}">
+                    </div>
+                    <div class="col-md-2">
+                        <input type="text" class="form-control form-control--custom m-input m_datepicker" placeholder="From Date"
+                            name="from" value="{{ (!empty($getData) ? (isset($getData['from'])?$getData['from']:'') : '') }}">
+                    </div>
+                    <div class="col-md-2">
+                        <input type="text" class="form-control form-control--custom m-input m_datepicker" placeholder="To Date"
+                            name="to" value="{{ (!empty($getData) ? (isset($getData['ro'])?$getData['to']:'') : '') }}">
+                    </div>
+                    <div class="col-md-3">
+                        <select class="form-control m-bootstrap-select m_selectpicker form-control--custom m-input" id="exampleSelect1"
+                            name="status">
+                            <option value="">All</option>
+                            @foreach(config('commanConfig.architect_applicationStatus') as $key=>$value)
+                            <option
+                                {{ (!empty($getData) ? (isset($getData['status'])?($getData['status']==$value?'selected':''):'') : '') }}
+                                value="{{$value}}">{{$key}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <button type="submit" class="btn m-btn--pill m-btn--custom btn-primary">Search</button>
+                        <button type="submit" name="reset" value="Reset" class="btn m-btn--pill m-btn--custom btn-metal">Reset</button>
+                    </div>
+                    <!-- <div class="col-md-6 mt-5">
                                 <div class="btn-list text-right">
                                     <button type="submit" name="excel" value="excel" class="btn excel-icon"><img src="{{asset('/img/excel-icon.svg')}}"></button>
                                     <a target="_blank" href=""
                                         class="btn print-icon"><img src="{{asset('/img/print-icon.svg')}}"></a>
                                 </div>
                             </div> -->
-                            
-                        </form>
-                        
-                        <!-- </div> -->
-                    </div>
-                </div>
+
+                </form>
+
+                <!-- </div> -->
             </div>
-            <!--end: Search Form -->
-            <!--begin: Datatable -->
+        </div>
+    </div>
+    <div class="m-portlet data-table--custom data-table--icons data-table--actions">
+        <div class="d-flex justify-content-between">
+            <h3 class="section-title section-title--small">Revision Requests</h3>
             <div class="topnav">
-            <a>Revision Requests</a>
-              <div class="topnav-right">
-                <a class="{{isset($_GET['application_status'])?($_GET['application_status']==0?'active':''):''}}" href="?application_status=0">All</a>
-                <a class="{{isset($_GET['application_status'])?($_GET['application_status']==1?'active':''):''}}" href="?application_status=1">Shortlisted</a>
-                <a class="{{isset($_GET['application_status'])?($_GET['application_status']==2?'active':''):''}}"  href="?application_status=2">Final</a>
-              </div>
-          </div>
-            @if($is_commitee==true)
-            <form method="post" action="{{route('finalise_architect_application')}}">
+                <a class="btn-link {{isset($_GET['application_status'])?($_GET['application_status']==0?'active':''):''}}"
+                    href="?application_status=0">All</a>
+                <a class="btn-link {{isset($_GET['application_status'])?($_GET['application_status']==1?'active':''):''}}"
+                    href="?application_status=1">Shortlisted</a>
+                <a class="btn-link {{isset($_GET['application_status'])?($_GET['application_status']==2?'active':''):''}}"
+                    href="?application_status=2">Final</a>
+            </div>
+        </div>
+        @if($is_commitee==true)
+        <form method="post" action="{{route('finalise_architect_application')}}">
             @else
             <form method="post" action="{{route('shortlist_architect_application')}}">
-            @endif
-              @csrf
-              @if($is_view==true)
-              <div class="col-md-6 mt-5">
-                  <button type="submit" name="shortlist" value="shortlist" class="btn btn-primary">Shortlist</button>
-                  <button type="submit" name="remove_shortlist" value="remove_shortlist" class="btn btn-primary">Remove from Shortlisted Lis</button>
-              </div>
-              @endif 
-              @if($is_commitee==true)
-              <div class="col-md-6 mt-5">
-                  <button type="submit" name="final" value="final" class="btn btn-primary">Add to Final list</button>
-                  <button type="submit" name="remove_final" value="remove_final" class="btn btn-primary">Remove from  Final list</button>
-              </div>
-              @endif
-              {!! $html->table() !!}
-            <!--end: Datatable -->
+                @endif
+                @csrf
+                @if($is_view==true)
+                <div class="btn-list mb-2">
+                    <button type="submit" name="shortlist" value="shortlist" class="btn btn-primary">Shortlist</button>
+                    <button type="submit" name="remove_shortlist" value="remove_shortlist" class="btn btn-primary">Remove
+                        from Shortlisted Lis</button>
+                </div>
+                @endif
+                @if($is_commitee==true)
+                <div class="btn-list mb-2">
+                    <button type="submit" name="final" value="final" class="btn btn-primary">Add to Final list</button>
+                    <button type="submit" name="remove_final" value="remove_final" class="btn btn-primary">Remove
+                        from Final list</button>
+                </div>
+                @endif
+                {!! $html->table() !!}
             </form>
-            
-        </div>
     </div>
     <input type="hidden" id="myModalBtn" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" />
 
