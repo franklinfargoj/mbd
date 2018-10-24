8f30fea3921fa92bf3af7d495e3979fb32e2773c
Bhavana Salunkhe <bhavna.salunkhe@neosofttech.com>
2018-10-24 14:52:26 +0530

    sidebar changes

--- resources/views/admin/cap_department/action.blade.php
+++ resources/views/admin/cap_department/action.blade.php
@@ -2,7 +2,7 @@
 $route="";
 $route=\Request::route()->getName();
 @endphp
-<li class="m-menu__item" data-toggle="collapse" data-target="#cap-actions">
+<li class="m-menu__item m-menu__item--submenu m-menu__item--level-2" data-toggle="collapse" data-target="#cap-actions">
     <a href="javascript:void(0);" class="m-menu__link m-menu__toggle">
         <i class="m-menu__link-icon flaticon-line-graph"></i>
         <span class="m-menu__link-title">
@@ -17,7 +17,7 @@ $route=\Request::route()->getName();
 </li>
 <li id="cap-actions" class="collapse show">
     <ul class="list-unstyled">
-        <li class="m-menu__item m-menu__item--submenu {{($route=='cap.view_application')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='cap.view_application')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" title="View Application" href="{{ route('cap.view_application', $ol_application->id) }}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
@@ -27,7 +27,7 @@ $route=\Request::route()->getName();
             </a>
         </li>
 
-        <li class="m-menu__item m-menu__item--submenu {{($route=='cap.society_EE_documents')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='cap.society_EE_documents')?'m-menu__item--active':''}}">
             <a class="m-menu__link" title="Society & EE Documents" href="{{route('cap.society_EE_documents',$ol_application->id)}}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
@@ -37,7 +37,7 @@ $route=\Request::route()->getName();
             </a>
         </li>
 
-        <li class="m-menu__item m-menu__item--submenu {{($route=='cap.EE_scrutiny_remark')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='cap.EE_scrutiny_remark')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" title="EE Scrutiny & Remarks" href="{{route('cap.EE_scrutiny_remark',$ol_application->id)}}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
@@ -47,7 +47,7 @@ $route=\Request::route()->getName();
             </a>
         </li>
 
-        <li class="m-menu__item m-menu__item--submenu {{($route=='cap.dyce_Scrutiny_Remark')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='cap.dyce_Scrutiny_Remark')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" title="DyCE Scrutiny & Remarks" href="{{route('cap.dyce_Scrutiny_Remark',$ol_application->id)}}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
@@ -57,7 +57,7 @@ $route=\Request::route()->getName();
             </a>
         </li>
 
-        <li class="m-menu__item m-menu__item--submenu {{($route=='cap.show_calculation_sheet')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='cap.show_calculation_sheet')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" title="REE Calculation Sheet" href="{{route('cap.show_calculation_sheet',$ol_application->id)}}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
@@ -67,7 +67,7 @@ $route=\Request::route()->getName();
             </a>
         </li>
 
-        <li class="m-menu__item m-menu__item--submenu {{($route=='cap.cap_notes')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='cap.cap_notes')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" title="CAP Notes" href="{{route('cap.cap_notes',$ol_application->id)}}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
@@ -77,7 +77,7 @@ $route=\Request::route()->getName();
             </a>
         </li>
 
-        <li class="m-menu__item m-menu__item--submenu {{($route=='cap.forward_application')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='cap.forward_application')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" title="Forward Application" href="{{route('cap.forward_application',$ol_application->id)}}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
--- resources/views/admin/ee_department/action.blade.php
+++ resources/views/admin/ee_department/action.blade.php
@@ -2,7 +2,7 @@
 $route="";
 $route=\Request::route()->getName();
 @endphp
-<li class="m-menu__item" data-toggle="collapse" data-target="#ee-actions">
+<li class="m-menu__item m-menu__item--submenu m-menu__item--level-2" data-toggle="collapse" data-target="#ee-actions">
     <a href="javascript:void(0);" class="m-menu__link m-menu__toggle">
         <i class="m-menu__link-icon flaticon-line-graph"></i>
         <span class="m-menu__link-title">
@@ -17,7 +17,7 @@ $route=\Request::route()->getName();
 </li>
 <li id="ee-actions" class="collapse show">
     <ul class="list-unstyled">
-        <li class="m-menu__item m-menu__item--submenu {{($route=='ee.view_application')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='ee.view_application')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" title="view_Application" href="{{ route('ee.view_application', $ol_application->id) }}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
@@ -27,7 +27,7 @@ $route=\Request::route()->getName();
             </a>
         </li>
 
-        <li class="m-menu__item m-menu__item--submenu {{($route=='document-submitted')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='document-submitted')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" href="{{ route('document-submitted', $ol_application->id) }}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
@@ -37,7 +37,7 @@ $route=\Request::route()->getName();
             </a>
         </li>
 
-        <li class="m-menu__item m-menu__item--submenu {{($route=='scrutiny-remark')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='scrutiny-remark')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" href="{{ route('scrutiny-remark', [$ol_application->id, $ol_application->society_id]) }}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
@@ -46,7 +46,7 @@ $route=\Request::route()->getName();
                 <span class="m-menu__link-text">Scrutiny & Remarks</span>
             </a>
         </li>
-        <li class="m-menu__item m-menu__item--submenu {{($route=='get-forward-application')?'m-menu__item--active':''}}">
+        <li class="m-menu__item m-menu__item--submenu m-menu__item--level-3 {{($route=='get-forward-application')?'m-menu__item--active':''}}">
             <a class="m-menu__link m-menu__toggle" href="{{ route('get-forward-application', $ol_application->id) }}">
                 <svg class="radio-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510">
                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
