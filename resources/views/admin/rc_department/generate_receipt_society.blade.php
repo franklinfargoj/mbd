3f4a43ef8230f538aaaeedd94ebbe37e305b6ee3
rohan <rohan.patil@wwindia.com>
2018-10-31 12:37:42 +0530

    Generate receipt RC.

--- resources/views/admin/rc_department/generate_receipt_society.blade.php
+++ resources/views/admin/rc_department/generate_receipt_society.blade.php
@@ -213,11 +213,12 @@
     $(document).ready(function () {
         /* Multi select with search data code start here */
         var json2 = JSON.parse('<?php echo $buildings; ?>');
-        //console.log(json2);
+        console.log(json2);
         $('.dropdown-sin-2').dropdown({
           data: json2,
           input: '<input type="text" maxLength="20" placeholder="Search">'
         });
+
         /* Multi select with search data code ends here */
 
         /* Multi select with search data code toggle payment mode start here */
