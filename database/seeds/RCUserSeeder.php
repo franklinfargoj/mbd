5056f0b90bc3337c6b47b7efb9b73be24865ead4
sudesh <sudesh.jadhav@neosofttech.com>
2018-11-06 19:46:53 +0530

    bug fix

--- database/seeds/RCUserSeeder.php
+++ database/seeds/RCUserSeeder.php
@@ -1,13 +1,14 @@
 <?php
 
-use Illuminate\Database\Seeder;
-use App\MasterLayout;
 use App\LayoutUser;
+use App\MasterLayout;
+use App\Permission;
+use App\PermissionRole;
 use App\Role;
 use App\RoleUser;
 use App\User;
-use App\Permission;
-use App\PermissionRole;
+use Illuminate\Database\Seeder;
+
 class RCUserSeeder extends Seeder
 {
     /**
@@ -17,19 +18,111 @@ class RCUserSeeder extends Seeder
      */
     public function run()
     {
+        $rc_permissions = [
+            [
+                'name' => 'rc.index',
+                'display_name' => 'List Collected Rents',
+                'description' => 'Listing Collected Rents',
+            ],
+            [
+                'name' => 'bill_collection_society',
+                'display_name' => 'Bill Collection Society',
+                'description' => 'Bill Collection Society',
+            ],
+            [
+                'name' => 'bill_collection_tenant',
+                'display_name' => 'Bill Collection Tenant',
+                'description' => 'Bill Collection Tenant',
+            ],
+            [
+                'name' => 'get_wards',
+                'display_name' => 'Get Wards Select Data',
+                'description' => 'Get Wards Select Data',
+            ],
+            [
+                'name' => 'get_colonies',
+                'display_name' => 'Get Colonies Select Data',
+                'description' => 'Get Colonies Select Data',
+            ],
+            [
+                'name' => 'get_society_select',
+                'display_name' => 'Get Societies Select Data',
+                'description' => 'Get Societies Select Data',
+            ],
+            [
+                'name' => 'get_building_select',
+                'display_name' => 'Selected Building',
+                'description' => 'Selected Building',
+            ],
+            [
+                'name' => 'arrears_calculations',
+                'display_name' => 'Arrears Calculations',
+                'description' => 'Arrears Calculations',
+            ],
+            [
+                'name' => 'billing_calculations',
+                'display_name' => 'Billing Calculations',
+                'description' => 'Billing Calculations',
+            ],
+            [
+                'name' => 'get_building_bill_collection',
+                'display_name' => 'Get Buildings Bill Collection List Data',
+                'description' => 'Get Buildings Bill Collection List Data',
+            ],
+            [
+                'name' => 'get_tenant_bill_collection',
+                'display_name' => 'Get Tenant Bill Collection List Data',
+                'description' => 'Get Tenant Bill Collection List Data',
+            ],
+            [
+                'name' => 'get_building_bill_collection',
+                'display_name' => 'Building Bill Collection',
+                'description' => 'Building Bill Collection',
+            ],
+            [
+                'name' => 'get_tenant_bill_collection',
+                'display_name' => 'Tenant Bill Collection',
+                'description' => 'Tenant Bill Collection',
+            ],
+            [
+                'name' => 'generate_receipt_society',
+                'display_name' => 'Generate Receipt Society',
+                'description' => 'Generate Receipt Society',
+            ],
+            [
+                'name' => 'generate_receipt_tenant',
+                'display_name' => 'Generate Receipt Tenant',
+                'description' => 'Generate Receipt Tenant',
+            ],
+            [
+                'name' => 'payment_receipt_society',
+                'display_name' => 'payment receipt society',
+                'description' => 'payment receipt society',
+            ],
+            [
+                'name' => 'payment_receipt_tenant',
+                'display_name' => 'payment receipt tenant',
+                'description' => 'payment receipt tenant',
+            ],
+        ];
         //RC
-        $rc_collector = Role::where('name', '=', 'rc_collector')->select('id')->get();
-
-        if (count($rc_collector) == 0) {
-
+        $rc_collector = Role::where('name', '=', 'rc_collector')->first();
+        if ($rc_collector) {
+            $rc_role_id = $rc_collector->id;
+        } else {
             $rc_role_id = Role::insertGetId([
                 'name' => 'rc_collector',
                 'redirect_to' => '/rc',
-                'parent_id' => NULL,
+                'parent_id' => null,
                 'display_name' => 'RC Collector',
-                'description' => 'RC Collector'
+                'description' => 'RC Collector',
             ]);
+        }
 
+        $rc_user = User::where(['email' => 'rc@gmail.com'])->first();
+        if ($rc_user) {
+            $rc_user_id = $rc_user->id;
+        } else {
             $rc_user_id = User::insertGetId([
                 'name' => 'Amit Kadam',
                 'email' => 'rc@gmail.com',
@@ -37,121 +130,52 @@ class RCUserSeeder extends Seeder
                 'role_id' => $rc_role_id,
                 'uploaded_note_path' => 'Test',
                 'mobile_no' => '7412589635',
-                'address' => 'Mumbai'
+                'address' => 'Mumbai',
             ]);
+        }
 
+        if (RoleUser::where(['user_id' => $rc_user_id, 'role_id' => $rc_role_id])->first()) {
+
+        } else {
             $role_user = RoleUser::insert([
                 'user_id' => $rc_user_id,
                 'role_id' => $rc_role_id,
-                'start_date' => \Carbon\Carbon::now()
+                'start_date' => \Carbon\Carbon::now(),
             ]);
+        }
 
-            $rc_permissions = [
-                [
-                    'name' => 'rc.index',
-                    'display_name' => 'List Collected Rents',
-                    'description' => 'Listing Collected Rents'
-                ],
-                [
-                    'name' => 'bill_collection_society',
-                    'display_name' => 'Bill Collection Society',
-                    'description' => 'Bill Collection Society'
-                ],
-                [
-                    'name' => 'bill_collection_tenant', 
-                    'display_name' => 'Bill Collection Tenant',
-                    'description' => 'Bill Collection Tenant'
-                ],
-                [
-                    'name' => 'get_wards', 
-                    'display_name' => 'Get Wards Select Data',
-                    'description' => 'Get Wards Select Data'
-                ],
-                [
-                    'name' => 'get_colonies', 
-                    'display_name' => 'Get Colonies Select Data',
-                    'description' => 'Get Colonies Select Data'
-                ],
-                [
-                    'name' => 'get_society_select', 
-                    'display_name' => 'Get Societies Select Data',
-                    'description' => 'Get Societies Select Data'
-                ],
-                [
-                    'name' => 'get_building_select',
-                    'display_name' => 'Selected Building',
-                    'description' => 'Selected Building'
-                ],
-                [
-                    'name' => 'arrears_calculations',
-                    'display_name' => 'Arrears Calculations',
-                    'description' => 'Arrears Calculations'
-                ],
-                [
-                    'name' => 'billing_calculations',
-                    'display_name' => 'Billing Calculations',
-                    'description' => 'Billing Calculations'
-                ],
-                [
-                    'name' => 'get_building_bill_collection', 
-                    'display_name' => 'Get Buildings Bill Collection List Data',
-                    'description' => 'Get Buildings Bill Collection List Data'
-                ],
-                [
-                    'name' => 'get_tenant_bill_collection', 
-                    'display_name' => 'Get Tenant Bill Collection List Data',
-                    'description' => 'Get Tenant Bill Collection List Data'
-                ],
-                [
-                    'name' => 'get_building_bill_collection',
-                    'display_name' => 'Building Bill Collection',
-                    'description' => 'Building Bill Collection'
-                ],
-                [
-                    'name' => 'get_tenant_bill_collection',
-                    'display_name' => 'Tenant Bill Collection',
-                    'description' => 'Tenant Bill Collection'
-                ],
-                [
-                    'name' => 'generate_receipt_society',
-                    'display_name' => 'Generate Receipt Society',
-                    'description' => 'Generate Receipt Society'
-                ],
-                [
-                    'name' => 'generate_receipt_tenant',
-                    'display_name' => 'Generate Receipt Tenant',
-                    'description' => 'Generate Receipt Tenant'
-                ],
-                [
-                    'name' => 'payment_receipt_society',
-                    'display_name' => 'payment receipt society',
-                    'description' => 'payment receipt society'
-                ],
-                [
-                    'name' => 'payment_receipt_tenant',
-                    'display_name' => 'payment receipt tenant',
-                    'description' => 'payment receipt tenant'
-                ]
-            ];
+        $permission_role = [];
 
-            $permission_role = [];
+        foreach ($rc_permissions as $rc) {
 
-            foreach ($rc_permissions as $rc ) {
-                                 
-                $rc_permission_id = Permission::insertGetId($rc);   
-                               
+            $rc_permission = Permission::where(['name' => $rc['name']])->first();
+            if ($rc_permission) {
+                $rc_permission_id = $rc_permission->id;
+            } else {
+
+                $rc_permission_id = Permission::insertGetId($rc);
+            }
+            if (PermissionRole::where(['permission_id' => $rc_permission_id, 'role_id' => $rc_role_id])->first()) {
+
+            } else {
                 $rc_permission_role[] = [
                     'permission_id' => $rc_permission_id,
                     'role_id' => $rc_role_id,
                 ];
             }
 
+        }
+        if (count($rc_permission_role) > 0) {
             PermissionRole::insert($rc_permission_role);
-            
-            $layout_id = MasterLayout::first()->id;
-            // Layout User Mapping
+        }
 
+        $layout_id = MasterLayout::first()->id;
+        // Layout User Mapping
+        if (LayoutUser::where(['user_id' => $rc_user_id, 'layout_id' => $layout_id])->first()) {
+
+        } else {
             LayoutUser::insert(['user_id' => $rc_user_id, 'layout_id' => $layout_id]);
         }
+
     }
 }
