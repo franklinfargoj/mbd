ab7356dece23aee94796df63e3bb6974beaf4605
Martin Philipose <martin.philipose@wwindia.com>
2018-11-13 15:43:31 +0530

    Worked on society conveyance section.

--- database/seeds/EmPermissionSeeder.php
+++ database/seeds/EmPermissionSeeder.php
@@ -306,6 +306,11 @@ class EmPermissionSeeder extends Seeder
                 'name' => 'create_society_bill',
                 'display_name' => 'Create Building Bill',
                 'description' => 'Create Building Bill'
+            ],
+            [
+                'name' => 'conveyance.view_documents',
+                'display_name' => 'View Documents',
+                'description' => 'View Documents Submitted'
             ]
         ];
 
