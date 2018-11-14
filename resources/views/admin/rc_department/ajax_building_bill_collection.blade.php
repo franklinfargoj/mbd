bb7c4ca420575a0123aefbf53f43ac9fe13920c1
rohan <rohan.patil@wwindia.com>
2018-11-14 22:22:45 +0530

    UI Changes

--- resources/views/admin/rc_department/ajax_building_bill_collection.blade.php
+++ resources/views/admin/rc_department/ajax_building_bill_collection.blade.php
@@ -21,21 +21,21 @@
                     {!! Form::open(['method' => 'get', 'route' => 'billing_calculations']) !!}
                     {{ Form::hidden('building_id', $value->id) }}
                     {{ Form::hidden('society_id', $value->society_id) }}
-                    {{ Form::button('<span class="btn-icon btn-icon--edit"><img src="img/view-icon.svg"></span> View Billing Details', array('class'=>'d-flex flex-column align-items-center','type'=>'submit')) }}
+                    {{ Form::button('<span class="btn-icon btn-icon--edit"><img src="/img/view-billing-details-icon.svg"></span> View Billing Details', array('class'=>'btn btn--unstyled p-0 btn--icon-wrap d-flex flex-column align-items-center','type'=>'submit')) }}
                     {!! Form::close() !!}
 
                     {!! Form::open(['method' => 'get', 'route' => 'generate_receipt_society']) !!}
                     {{ Form::hidden('building_id', $value->id) }}
                     {{ Form::hidden('society_id', $value->society_id) }}     
                     <div class="d-flex btn-icon-list">
-                    {{ Form::button('<span class="btn-icon btn-icon--edit"><img src="img/view-icon.svg"></span>Generate Reciept', array('class'=>'d-flex flex-column align-items-center','type'=>'submit')) }}
+                    {{ Form::button('<span class="btn-icon btn-icon--edit"><img src="/img/generate-bill-icon.svg"></span>Generate Reciept', array('class'=>'btn btn--unstyled p-0 btn--icon-wrap d-flex flex-column align-items-center','type'=>'submit')) }}
                     </div>
                     {!! Form::close() !!}
                     
                     {!! Form::open(['method' => 'get', 'route' => 'view_bill_building']) !!}
                     {{ Form::hidden('building_id', $value->id) }}
                     {{ Form::hidden('society_id', $value->society_id) }}                   
-                    {{ Form::button('<span class="btn-icon btn-icon--edit"><img src="img/view-icon.svg"></span>View Bill', array('class'=>'d-flex flex-column align-items-center','type'=>'submit')) }}
+                    {{ Form::button('<span class="btn-icon btn-icon--edit"><img src="/img/view-arrears-calculation-icon.svg"></span>View Bill', array('class'=>'btn btn--unstyled p-0 btn--icon-wrap d-flex flex-column align-items-center','type'=>'submit')) }}
                     {!! Form::close() !!}
 
                 </td>
