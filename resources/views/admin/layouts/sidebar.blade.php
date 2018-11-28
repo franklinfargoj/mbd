73f093ef20acdca4aeec9be58f20b510125b5b67
amar.prajapati <amar.prajapati@neosofttech.com>
2018-11-28 15:22:42 +0530

    Changes in land module.

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -341,7 +341,7 @@ $route=\Request::route()->getName();
                             </ul>
                         </li>
 
-                        @if(\Illuminate\Support\Facades\Request::is('lease_detail/*') || (strpos($route,'village_detail') !== false) || (strpos($route,'architect_layouts') !== false) || (strpos($route,'society_detail') !== false))
+                        @if(\Illuminate\Support\Facades\Request::is('lease_detail/*') || (strpos($route,'village_detail') !== false) || (strpos($route,'architect_layout') !== false) || (strpos($route,'society_detail') !== false))
                         <li class="m-menu__item m-menu__item--level-2 {{($route=='lease_detail.index' || $route=='view-lease.view' || $route=='edit-lease.edit' || $route=='lease_detail.create')? '' : 'collapsed'}}"
                             data-toggle="collapse" data-target="#lease-actions">
                             <a href="{{url('/village_detail')}}" class="m-menu__link m-menu__toggle">
@@ -391,7 +391,7 @@ $route=\Request::route()->getName();
                                                     <path d="M255 127.5c-71.4 0-127.5 56.1-127.5 127.5S183.6 382.5 255 382.5 382.5 326.4 382.5 255 326.4 127.5 255 127.5zM255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
                                                           fill="#FFF" />
                                                 </svg>
-                                                <span class="m-menu__link-text">Lease Details</span></a>
+                                                <span class="m-menu__link-text">List of Lease</span></a>
                                         </li>
 
                                         @if((strpos($route,'lease_detail') !== false))
