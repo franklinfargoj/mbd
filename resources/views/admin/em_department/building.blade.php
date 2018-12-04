b90e74e07c328ed9846152c8b70590ac6895daaf
sonali.raut@wwindia.com <sonali.raut@wwindia.com>
2018-12-04 13:05:41 +0530

    building and tenant table converted to yajra table

--- resources/views/admin/em_department/building.blade.php
+++ resources/views/admin/em_department/building.blade.php
@@ -65,14 +65,6 @@
     <!-- END: Subheader -->
     <div class="m-portlet m-portlet--compact m-portlet--mobile">
         <div class="m-portlet__head">
-            <div class="m-portlet__head-caption">
-                <div class="m-portlet__head-text">
-                    {{-- <h3 class="m-portlet__head-text"> List of buildings </h3> --}}
-                    <div id="dataTableBuilder_filter" class="col-md-4 ml-auto pull-left"><input type="search" id="searchId" class="form-control input-sm input-small input-inline form-control--custom"
-                    placeholder="Search ..."></div>    
-                </div>
-            </div>
-
             <div class='btn-icon-list'>
                 <a href="{{route('add_building', [$society_id])}}" class='btn m-btn--pill m-btn--custom btn-primary pull-right' style="">Add Building</a>
             </div>
@@ -80,45 +72,7 @@
            </div>
         <div class="m-portlet__body">
             <!--begin: Datatable -->
-        <table id="example" class="display table table-responsive table-bordered" style="width:100%">
-        <thead>
-            <tr>
-                <th>Sr. No.</th>
-                <th>Building / Chawl Number</th>
-                <th>Building / Chawl Name</th>
-                <th>Number of Tenant</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody id="myTable">
-        <?php $row_no = 1; ?>
-        @foreach($buildings as $key => $value )
-            <tr>
-                <td>{{$row_no++}}</td>
-                <td>{{$value->building_no}}</td>
-                <td>{{$value->name}}</td>
-                <td><?php echo isset($value->tenant_count[0]->count) ? $value->tenant_count[0]->count : '0'; ?></td>
-                <td>
-                    <div class='d-flex btn-icon-list'>
-                        <a href="{{route('get_tenants', [encrypt($value->id)])}}" class='d-flex flex-column align-items-center ' style="padding-left: 5px; padding-right: 5px; text-decoration: none; color: #212529; font-size:12px;" ><span class='btn-icon btn-icon--view'><img src="{{asset('/img/view-icon.svg')}}"></span>Tenant Details</a>
-                    
-                        <a href="{{route('edit_building', [encrypt($value->id)])}}" class='d-flex flex-column align-items-center' style="padding-left: 5px; padding-right: 5px; text-decoration: none; color: #212529; font-size:12px;"><span class='btn-icon btn-icon--edit'><img src="{{asset('/img/edit-icon.svg')}}"></span>Edit</a>
-                    </div>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>Sr. No.</th>
-                <th>Building / Chawl Number</th>
-                <th>Building / Chawl Name</th>
-                <th>Number of Tenant</th>
-                <th>Action</th>
-            </tr>
-        </tfoot>
-        </table>
-        {!! $buildings->render() !!}
+            {!! $html->table() !!}
             <!--end: Datatable -->
         </div>
     </div>
@@ -132,7 +86,7 @@
 </div>
 @endsection
 @section('datatablejs')
-
+{!! $html->scripts() !!}
 
 <script>
     /*$("#update_status").on("change", function () {
@@ -156,7 +110,7 @@
                     data: {search: value},
                         success: function(response){
                         console.log(response);
-                        $('.m-portlet__body').html(response);
+                        // $('.m-portlet__body').html(response);
                         //$('#colony').selectpicker('refresh');
                     }
             });                
