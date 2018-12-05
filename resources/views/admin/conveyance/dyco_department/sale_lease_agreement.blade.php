173f4ee95f078c069afcfd1d1c3958015a2c6dfe
Bhavana Salunkhe <bhavna.salunkhe@neosofttech.com>
2018-12-05 12:06:50 +0530

    conveyance application

--- resources/views/admin/conveyance/dyco_department/sale_lease_agreement.blade.php
+++ resources/views/admin/conveyance/dyco_department/sale_lease_agreement.blade.php
@@ -46,13 +46,6 @@
             <div class="m-portlet m-portlet--tabs m-portlet--bordered-semi mb-0 m-portlet--shadow">
                 <div class="portlet-body">
                     <div class="m-portlet__body m-portlet__body--table">
-                        <div class="m-subheader" style="padding: 0;">
-                            <div class="d-flex align-items-center justify-content-center">
-                                <h4 class="section-title">
-                                    Sale Deed Agreement
-                                </h4>
-                            </div>
-                        </div>
                         <div class="m-section__content mb-0 table-responsive">
                             <div class="container">
                                 <div class="row">
@@ -100,13 +93,6 @@
             <div class="m-portlet m-portlet--tabs m-portlet--bordered-semi mb-0 m-portlet--shadow">
                 <div class="portlet-body">
                     <div class="m-portlet__body m-portlet__body--table">
-                        <div class="m-subheader" style="padding: 0;">
-                            <div class="d-flex align-items-center justify-content-center">
-                                <h4 class="section-title">
-                                    Lease Deed Agreement
-                                </h4>
-                            </div>
-                        </div>
                         <div class="m-section__content mb-0 table-responsive">
                             <div class="container">
                                 <div class="row">
