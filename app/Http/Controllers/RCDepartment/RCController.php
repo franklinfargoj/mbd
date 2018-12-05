0ed43c7899c4f5c39f920d9329605e35c7158f77
sonali.raut <sonali.raut@wwindia.com>
2018-12-05 19:06:36 +0530

    adding change

--- app/Http/Controllers/RCDepartment/RCController.php
+++ app/Http/Controllers/RCDepartment/RCController.php
@@ -525,9 +525,10 @@ class RCController extends Controller
             
             $data['consumer_number'] = substr(sprintf('%08d', $data['building']->society_id),0,8).'|'.substr(sprintf('%08d', $data['building']->id),0,8);
             if(true == $is_download) {
-              $pdf = PDF::loadView('admin.rc_department.download_building_bill', $data);
-              // print_r($pdf);exit;
-              return $pdf->download('bill_'.$data['building']->name.'_'.$data['building']->building_no.'.pdf');
+              return return view('admin.rc_department.download_building_bill.blade', $data);
+              // $pdf = PDF::loadView('admin.rc_department.download_building_bill', $data);
+              // // print_r($pdf);exit;
+              // return $pdf->download('bill_'.$data['building']->name.'_'.$data['building']->building_no.'.pdf');
             } else {
               return view('admin.rc_department.view_bill_building',$data);
             }
@@ -567,8 +568,9 @@ class RCController extends Controller
             $data['consumer_number'] = substr(sprintf('%08d', $data['building']->id),0,8).'|'.substr(sprintf('%08d', $data['tenant']->id),0,8);
             $data['is_download'] = $is_download;
             if(true == $is_download) {
-              $pdf = PDF::loadView('admin.rc_department.download_tenant_bill.blade', $data);
-              return $pdf->download('bill_'.$data['building']->name.'_'.$data['building']->building_no.'.pdf');
+                return view('admin.rc_department.download_tenant_bill.blade', $data);
+              // $pdf = PDF::loadView('admin.rc_department.download_tenant_bill.blade', $data);
+              // return $pdf->download('bill_'.$data['building']->name.'_'.$data['building']->building_no.'.pdf');
             } else {
                 return view('admin.rc_department.view_bill_tenant',$data);
             }
