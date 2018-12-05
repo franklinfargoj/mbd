9384b35cc49b9a9e77c5e9269ce68f1b03b59bf5
sudesh <sudesh.jadhav@neosofttech.com>
2018-12-05 20:50:53 +0530

    society formation bugs fixed

--- resources/views/admin/formation/view_application.blade.php
+++ resources/views/admin/formation/view_application.blade.php
@@ -13,8 +13,8 @@
                 {{ Breadcrumbs::render('sf_view_application',$sf_application->id) }}
                 <div class="ml-auto btn-list">
                     <a href="{{ url()->previous() }}" class="btn btn-link"><i class="fa fa-long-arrow-left" style="padding-right: 8px;"></i>Back</a>
-                    <a href="#" target="_blank" id="download_application_form" class="btn print-icon" rel="noopener"
-                       onclick="printContent('printdiv')"><img src="{{asset('/img/print-icon.svg')}}" title="print"></a>
+                    <a href="?print=1" target="_blank" class="btn print-icon" rel="noopener"
+                       ><img src="{{asset('/img/print-icon.svg')}}" title="print"></a>
                 </div>
             </div>
         </div>
