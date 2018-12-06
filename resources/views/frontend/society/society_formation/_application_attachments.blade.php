bf3168546fe828c839b8f00c1ae473636c014bf2
sudesh <sudesh.jadhav@neosofttech.com>
2018-12-06 16:05:04 +0530

    society formation all documents required validation added

--- resources/views/frontend/society/society_formation/_application_attachments.blade.php
+++ resources/views/frontend/society/society_formation/_application_attachments.blade.php
@@ -1,5 +1,10 @@
 <table class="table">
-    @php $i=1; @endphp
+    @php $i=1; 
+        if(session()->has( 'doc_errors' ))
+        {
+            $doc_errors=$id = session()->get('doc_errors');
+        }
+    @endphp
 <input type="hidden" name="sf_application_id" id="sf_application_id" value="{{$sf_application->id}}">
     @foreach ($sf_documents as $item)
     <tr>
@@ -22,9 +27,10 @@
                 $file="";
                 $file=$item->sf_document_status!=""?$item->sf_document_status->document_path:'';
                 @endphp
+                
             <a style="display:{{$file!=""?'block':'none'}}" target="_blank" id="uploaded_file_{{$i}}" class="btn-link" href="{{$file!=""?config('commanConfig.storage_server').'/'.$file:''}}">download</a>
             {{-- <a class="btn-link" target="_blank" style="display:none;" id="uploaded_file_{{$i}}" href="">download</a> --}}
-            <span class="text-danger" id="sf_doc_error_{{$i}}"></span>
+            <span class="text-danger" id="sf_doc_error_{{$i}}">{{isset($doc_errors[$item->id])?$doc_errors[$item->id]:''}}</span>
         </td>
     </tr>
     @php $i++; @endphp
