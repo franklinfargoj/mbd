84b75e37a557dd83e8674348c8e4bb6e3e9a7be2
prajakta.sisale <prajakta.sisale@neosoftech.com>
2018-12-10 16:34:41 +0530

    added pie chart to co dashboard

--- resources/views/admin/co_department/dashboard.blade.php
+++ resources/views/admin/co_department/dashboard.blade.php
@@ -22,50 +22,128 @@
             </div>
             <div class="m-portlet__body m-portlet__body--hearing m-portlet__body--spaced collapse show" id="co-ol-summary"
                  data-parent="#accordion">
-                <div class="row hearing-row">
-                    @foreach($dashboardData as $header => $value)
-                        <div class="col">
-                            <div class="m-portlet app-card text-center">
-                                <h2 class="app-heading">{{$header}}</h2>
-                                <h2 class="app-no mb-0">{{$value[0]}}</h2>
-                                <a href="{{session()->get('redirect_to').$value[1]}}" class="app-card__details mb-0">View Details</a>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-
-        @if($dashboardData1)
-            <div class="hearing-accordion-wrapper">
-                <div class="m-portlet m-portlet--compact hearing-accordion mb-0">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <a class="btn--unstyled section-title section-title--small d-flex justify-content-between mb-0 w-100"
-                           data-toggle="collapse" href="#co-ol-pending-summary">
-                            <span class="form-accordion-title">REE Offer Letter Pending Applications Summary</span>
-                            <span class="accordion-icon"></span>
-                        </a>
-                    </div>
-                </div>
-                <div class="m-portlet__body m-portlet__body--hearing m-portlet__body--spaced collapse show" id="ree-ol-pending-summary"
+                <div class="m-portlet__body m-portlet__body--hearing m-portlet__body--spaced collapse show" id="ree-ol-summary"
                      data-parent="#accordion">
                     <div class="row hearing-row">
-                        @foreach($dashboardData1 as $header => $value)
+                        @php $chart = 0;@endphp
+                        @foreach($dashboardData as $header => $value)
                             <div class="col">
                                 <div class="m-portlet app-card text-center">
                                     <h2 class="app-heading">{{$header}}</h2>
-                                    <h2 class="app-no mb-0">{{$value}}</h2>
-                                    <a href="" class="app-card__details mb-0">View Details</a>
+                                    <h2 class="app-no mb-0">{{$value[0]}}</h2>
+                                    @php $chart += $value[0];@endphp
+                                    <a href="{{url(session()->get('redirect_to').$value[1])}}" class="app-card__details mb-0">View Details</a>
                                 </div>
                             </div>
                         @endforeach
                     </div>
+                    @if($chart)
+                        <div id="chartdiv" style="width: 100%; height: 350px; margin-top: 2px;"></div>
+                    @endif
+
+                    @if($dashboardData1)
+                        <div class="row hearing-row">
+                            @php $chart = 0;@endphp
+                            @foreach($dashboardData1 as $header => $value)
+                                <div class="col">
+                                    <div class="m-portlet app-card text-center">
+                                        <h2 class="app-heading">{{$header}}</h2>
+                                        <h2 class="app-no mb-0">{{$value}}</h2>
+                                        @php $chart += $value;@endphp
+                                        {{--<a href="" class="app-card__details mb-0">View Details</a>--}}
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                        @if($chart)
+                            <div id="chartdiv1" style="width: 100%; height: 350px; margin-top: 2px;"></div>
+                        @endif
+                    @endif
                 </div>
             </div>
-        @endif
+        </div>
     </div>
 
 @endsection
+@section('js')
+    <script type="text/javascript" src="{{ asset('/js/amcharts.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/pie.js') }}"></script>
+
+    <script>
+        var chart;
+        var legend;
+
+
+        var chartData = [
+
+                @foreach($dashboardData as $header => $value)
+                @if($header != 'Total No of Application'){
+                "status": '{{$header}}',
+                "value": '{{$value[0]}}',
+            },
+            @endif
+            @endforeach
+
+        ];
+
+        AmCharts.ready(function () {
+            // PIE CHART
+            chart = new AmCharts.AmPieChart();
+            chart.dataProvider = chartData;
+            chart.titleField = "status";
+            chart.valueField = "value";
+            chart.outlineColor = "#FFFFFF";
+            chart.outlineAlpha = 0.8;
+            chart.outlineThickness = 2;
+            chart.balloonText = "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>";
+            // this makes the chart 3D
+            chart.depth3D = 15;
+            chart.angle = 30;
+            chart.colors = [ "#f0791b", "#ffc063", "#2A0CD0", "#8bc34a", "#CD0D74", "#754DEB", "#DDDDDD", "#999999", "#333333", "#000000", "#57032A", "#CA9726", "#990000", "#4B0C25"]
+//
+            // WRITE
+            chart.write("chartdiv");
+        });
+    </script>
+    <script>
+        var chart1;
+        var legend;
+
+                @if($dashboardData1)
+        var chartData1 = [
+                        @foreach($dashboardData1 as $header => $value)
+                        @if($header != 'Total number of Application Pending'){
+                    "status": '{{$header}}',
+                    "value": '{{$value}}',
+                },
+                    @endif
+                    @endforeach
+            ];
+
+        AmCharts.ready(function () {
+            // PIE CHART
+            chart1 = new AmCharts.AmPieChart();
+            chart1.dataProvider = chartData1;
+            chart1.titleField = "status";
+            chart1.valueField = "value";
+            chart1.outlineColor = "#FFFFFF";
+            chart1.outlineAlpha = 0.8;
+            chart1.outlineThickness = 2;
+            chart1.balloonText = "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>";
+            // this makes the chart 3D
+            chart1.depth3D = 15;
+            chart1.angle = 30;
+            chart1.colors = [ "#f0791b", "#ffc063", "#2A0CD0", "#8bc34a", "#CD0D74", "#754DEB", "#DDDDDD", "#999999", "#333333", "#000000", "#57032A", "#CA9726", "#990000", "#4B0C25"]
+//
+            // WRITE
+            chart1.write("chartdiv1");
+        });
+        @endif
+
+    </script>
+@endsection
+
+
 {{--@section('js')--}}
 {{--<script>--}}
 {{--$(".accordion-icon").on("click", function () {--}}
