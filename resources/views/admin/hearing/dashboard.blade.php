dc1df30f0598413559096fb2a65ce81b69154091
prajakta.sisale <prajakta.sisale@neosoftech.com>
2018-12-13 14:33:06 +0530

    added pie chart for conveyance in hearing module

--- resources/views/admin/hearing/dashboard.blade.php
+++ resources/views/admin/hearing/dashboard.blade.php
@@ -111,6 +111,7 @@
             </div>
             <div class="m-portlet__body m-portlet__body--hearing m-portlet__body--spaced collapse show" id="conveyance-summary"
                  data-parent="#accordion">
+                @php $chart = 0; @endphp
                 <div class="row hearing-row">
                     @foreach($conveyanceDashboard[0] as $header => $value)
                         <div class="col">
@@ -118,7 +119,8 @@
                                 <h2 class="app-heading">{{$header}}</h2>
                                 <div class="app-card-footer">
                                     <h2 class="app-no mb-0">{{$value[0]}}</h2>
-                                    @if( $value[1] == 'pending')
+                                    @php $chart += 1; @endphp
+                                @if( $value[1] == 'pending')
                                         <a href="{{url($value[1])}}" class="app-card__details mb-0" data-toggle="modal" data-target="#pending">View Details</a>
                                     @elseif( $value[1] == 'sendToSociety')
                                         <a href="{{url($value[1])}}" class="app-card__details mb-0" data-toggle="modal" data-target="#sendToSociety">View Details</a>
@@ -131,18 +133,28 @@
                         </div>
                     @endforeach
                 </div>
+                @if($chart)
+                    <div id="conveyance_chart" style="width: 100%; height: 350px; margin-top: 2px;"></div>
+                @endif
+
                 @if($pendingApplications)
+                    @php $chart1 = 0; @endphp
                     <div class="row hearing-row">
                         @foreach($pendingApplications as $header => $value)
                             <div class="col">
                                 <div class="m-portlet app-card text-center">
                                     <h2 class="app-heading">{{$header}}</h2>
                                     <h2 class="app-no mb-0">{{$value}}</h2>
+                                    @php $chart1 += 1; @endphp
+
                                     {{--<a href="" class="app-card__details mb-0">View Details</a>--}}
                                 </div>
                             </div>
                         @endforeach
                     </div>
+                    @if($chart1)
+                        <div id="pending_conveyance_chart" style="width: 100%; height: 350px; margin-top: 2px;"></div>
+                    @endif
                 @endif
             </div>
         </div>
@@ -285,5 +297,73 @@
             // WRITE
             chart.write("chartdiv");
         });
+    </script>
+
+    <script>
+        var chart1;
+        var legend;
+
+        var chartData1 = [
+                @foreach($conveyanceDashboard[0] as $header => $value)
+            {
+                @if(!($header == 'Total No of Applications'))
+                    "status": "{{$header}}",
+                    "value": "{{$value[0]}}",
+                @endif
+            },
+            @endforeach
+        ];
+
+        AmCharts.ready(function () {
+            // PIE CHART
+            chart1 = new AmCharts.AmPieChart();
+            chart1.dataProvider = chartData1;
+            chart1.titleField = "status";
+            chart1.valueField = "value";
+            chart1.outlineColor = "#FFFFFF";
+            chart1.outlineAlpha = 0.8;
+            chart1.outlineThickness = 2;
+            chart1.balloonText = "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>";
+            // this makes the chart 3D
+            chart1.depth3D = 15;
+            chart1.angle = 30;
+            chart1.colors = [ "#f0791b", "#ffc063", "#2A0CD0", "#8bc34a", "#CD0D74", "#754DEB", "#DDDDDD", "#999999", "#333333", "#000000", "#57032A", "#CA9726", "#990000", "#4B0C25"]
+//
+            // WRITE
+            chart1.write("conveyance_chart");
+        });
+    </script>
+    <script>
+        var chart2;
+        var legend;
+        @if($pendingApplications)
+        var chartData2 = [
+                @foreach($pendingApplications as $header => $value)
+            {
+                "status": "{{$header}}",
+                "value": "{{$value}}",
+            },
+            @endforeach
+        ];
+
+        AmCharts.ready(function () {
+            // PIE CHART
+            chart2 = new AmCharts.AmPieChart();
+            chart2.dataProvider = chartData2;
+            chart2.titleField = "status";
+            chart2.valueField = "value";
+            chart2.outlineColor = "#FFFFFF";
+            chart2.outlineAlpha = 0.8;
+            chart2.outlineThickness = 2;
+            chart2.balloonText = "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>";
+            // this makes the chart 3D
+            chart2.depth3D = 15;
+            chart2.angle = 30;
+            chart2.colors = [ "#f0791b", "#ffc063", "#2A0CD0", "#8bc34a", "#CD0D74", "#754DEB", "#DDDDDD", "#999999", "#333333", "#000000", "#57032A", "#CA9726", "#990000", "#4B0C25"]
+//
+            // WRITE
+            chart2.write("pending_conveyance_chart");
+        });
+        @endif
     </script>
 @endsection
