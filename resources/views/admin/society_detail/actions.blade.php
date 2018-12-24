5636df7ffbbc90f3f4a8b3a8a9207cb3bd2485a2
prajakta.sisale <prajakta.sisale@neosoftech.com>
2018-12-24 11:12:01 +0530

    encryption of ids in society module

--- resources/views/admin/society_detail/actions.blade.php
+++ resources/views/admin/society_detail/actions.blade.php
@@ -1,7 +1,7 @@
 <div class="d-flex btn-icon-list">
-    <a class="d-flex flex-column align-items-center" href="{{ route('society_detail.show', $society_data->id) }}"><span
+    <a class="d-flex flex-column align-items-center" href="{{ route('society_detail.show', encrypt($society_data->id)) }}"><span
             class="btn-icon btn-icon--view"><img src="{{ asset('/img/view-icon.svg')}}"></span>View</a>
-    <a class="d-flex flex-column align-items-center" href="{{ route('society_detail.edit', $society_data->id) }}"><span
+    <a class="d-flex flex-column align-items-center" href="{{ route('society_detail.edit',  encrypt($society_data->id)) }}"><span
             class="btn-icon btn-icon--edit"><img src="{{ asset('/img/edit-icon.svg')}}"></span>Edit</a>
     <a class="d-flex flex-column align-items-center" href="{{ route('lease_detail.index', encrypt($society_data->id)) }}"><span
                 class="btn-icon btn-icon--delete"><i style="color: #fff;" class="fa fa-file-text-o lease-icon"></i></span>Lease</a>
