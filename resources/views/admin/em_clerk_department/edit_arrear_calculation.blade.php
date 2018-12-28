3a7a5d1c2de660441d75f3a394e80a26d34d1c21
sonali.raut <sonali.raut@wwindia.com>
2018-12-28 18:45:08 +0530

    adding change of issue society list

--- resources/views/admin/em_clerk_department/edit_arrear_calculation.blade.php
+++ resources/views/admin/em_clerk_department/edit_arrear_calculation.blade.php
@@ -268,7 +268,7 @@
 
 @section('datatablejs')
  {!! $html->scripts() !!}
-<script>
+{{-- <script>
     /*$("#update_status").on("change", function () {
         $("#eeForm").submit();
     });*/
@@ -353,6 +353,416 @@ function monthDiff(d1, d2) {
     return months <= 0 ? 0 : months;
 }
 
+
+</script> --}}
+<script>
+    /*$("#update_status").on("change", function () {
+        $("#eeForm").submit();
+    });*/
+    $(document).ready(function () {
+        $(".display_msg").delay(5000).slideUp(300);
+    });
+
+    $(document).on('change', '.ior', function(){
+        total_amount();
+    });
+
+    $(document).on('change', '.ida', function(){
+        total_amount();
+    });
+
+
+    var old_rate_old = 0;
+    var old_rate_diff = 0;
+    var old_iod = 0;
+    var old_ior = 0;
+    var old_intrest_amount1 = 0;
+    var old_intrest_amount2 = 0;
+    var intrest_on_difference1 = 0;
+    var intrest_on_difference2 = 0;
+    var old_intrest_amount = 0;
+    var intrest_on_difference = 0;
+    var iod_per = 0;
+    var ior_per = 0;
+    var old_rate = 0;
+    var ior = 0;
+    var iod = 0;
+    var rate_diff = 0
+    var total1 = 0 
+    var total = 0;
+    var total2 = 0;
+    var currentYear = '';
+    var Year = '';
+    var currentMonth = '';
+
+    function total_amount(){
+                
+                ior = "<?php echo $rate_card->interest_on_old_rate ?>";
+                old_rate = "<?php echo $rate_card->old_rate ?>";
+                
+                iod = "<?php echo $rate_card->interest_on_differance ?>";
+                rate_diff = "<?php echo $rate_card->revise_rate - $rate_card->old_rate ?>";
+                
+                var bill_year = $('#bill_year option:selected').val();
+                var bill_month = $('#bill_month option:selected').val();
+                // bill_month = bill_month - 1;
+                var ior_year = $('#ior_year option:selected').val();
+                var ior_month = $('#ior_month option:selected').val();
+                // if( ior_month != 1 ) {
+                    // ior_month = ior_month - 1;
+                // }
+
+                var ida_year = $('#ida_year option:selected').val();
+                var ida_month = $('#ida_month option:selected').val();
+
+                // ida_month = ida_month - 1;
+
+
+                var currentDate = new Date(),
+
+                currentMonth = currentDate.getMonth(),
+
+                currentYear = currentDate.getFullYear();
+
+                var building_id = "<?php  echo encrypt($tenant->building_id); ?>";
+                var society_id = "<?php  echo encrypt($society->id); ?>";
+
+                var year_diff = currentYear-ior_year;
+                // if(year_diff > 1) {
+                    var start_year = bill_year;
+                    var end_year = ior_year;
+
+                    $.ajax({
+                        url:"{{URL::route('get_arrear_charges_multiple')}}",
+                        type: 'get',
+                        data: {start_year: start_year,building_id:building_id,society_id:society_id,ior_year:ior_year,ior_month:ior_month, ida_month:ida_month,ida_year:ida_year},
+                            success: function(response){
+                            var strResponse = $.parseJSON(response);
+                            
+
+                            if (true == strResponse.result && null != strResponse.data && strResponse.data.length == Number(year_diff+1)) {
+                                old_rate_old = [];
+                                old_rate_diff = [];
+                                old_iod = [];
+                                old_ior = [];
+
+                                $.each(strResponse.data,function(i,v) {
+                                    old_rate_old[v.year] = Number(v.old_rate);
+                                    old_rate_diff[v.year] = Number(v.revise_rate) - Number(v.old_rate);
+                                    old_iod[v.year] = Number(v.interest_on_differance);
+                                    old_ior[v.year] = Number(v.interest_on_old_rate);
+                                });
+                                bill_month = bill_month - 1;
+                                var start_date = new Date(ior_year,ior_month,01);
+                                var end_date = new Date(bill_year,bill_month,01);
+
+                                var dates = dateRange(formatDate(start_date),formatDate(end_date));                    
+
+                                var start_date_int = new Date(ida_year, ida_month, 01);
+                                var end_date_int = new Date(bill_year, bill_month, 01);
+                                
+                                var datesint = dateRange(formatDate(start_date_int),formatDate(end_date_int));
+
+                                console.log(dates);
+
+                                if(null != dates ) {
+                                    var old_intrest_amount_temp = 0;
+                                    var intrest_on_difference_temp = 0;
+                                    var old_total = 0;
+                                    var old_total_intrest_on_difference = 0;
+
+                                    for(i=0; i < dates.length;i++) {
+                                        var monthlyDate = dates[i];
+                                        
+                                        var tempYear = new Date(monthlyDate).getFullYear();
+                                        var tempMonth = new Date(monthlyDate).getMonth();
+                                        
+                                        tempMonth = tempMonth +1;
+                                        
+
+                                        if(tempMonth > 3 ) {
+
+                                            old_ior_per = old_ior[tempYear] / 100;
+
+                                            var temp = parseFloat(old_rate_old[tempYear] * old_ior_per ).toFixed(2);
+                                        } else if(tempMonth <3 ) {
+                                            old_ior_per = old_ior[tempYear-1] / 100;
+
+                                            var temp = parseFloat(old_rate_old[tempYear-1] * old_ior_per ).toFixed(2);
+                                        } 
+
+                                        if(tempMonth > 3 ) {
+                                            
+                                            old_total += parseFloat(old_rate_old[tempYear]) + parseFloat(temp) || 0;
+                                        } else if(tempMonth <= 3) {
+                                            old_total += parseFloat(old_rate_old[tempYear-1]) + parseFloat(temp) || 0;
+                                        }
+                                        
+                                        old_intrest_amount_temp += parseFloat(temp) || 0;
+                                        
+                                    }
+                                    
+
+                                    for(i=0; i < datesint.length;i++) {
+                                        var monthlyDate = datesint[i];
+                                        
+                                        var tempYear = new Date(monthlyDate).getFullYear();
+                                        var tempMonth = new Date(monthlyDate).getMonth();
+                                        
+                                        tempMonth = tempMonth +1;
+                                        
+
+                                        if(tempMonth > 3 ) {
+
+                                            old_iod_per = old_iod[tempYear] / 100;
+
+                                            var temp1 = parseFloat(old_rate_diff[tempYear] * old_iod_per).toFixed(2);
+                                        } else if(tempMonth <3 ) {
+                                            old_iod_per = old_iod[tempYear-1] / 100;
+
+                                            var temp1 = parseFloat(old_rate_diff[tempYear-1] * old_iod_per).toFixed(2);
+                                        } 
+
+                                        if(tempMonth > 3 ) {
+                                            
+                                            old_total_intrest_on_difference += parseFloat(old_rate_diff[tempYear]) + parseFloat(temp1) || 0;
+                                        } else if(tempMonth <= 3) {
+                                            old_total_intrest_on_difference += parseFloat(old_rate_diff[tempYear-1]) + parseFloat(temp1) || 0;
+                                        }
+                                        
+                                        intrest_on_difference_temp += parseFloat(temp1) || 0;
+                                        
+                                    };
+                                    
+                                    old_intrest_amount = old_intrest_amount_temp;
+                                    intrest_on_difference = intrest_on_difference_temp;
+                                    total = old_total + old_total_intrest_on_difference;
+                                }
+
+                                $('#oia').html(old_intrest_amount);
+                                $('#old_intrest_amount').val(old_intrest_amount);
+
+                                $('#dia').html(intrest_on_difference);
+                                $('#difference_intrest_amount').val(intrest_on_difference);
+                            
+                                 $('#total_amount').html(total);
+                                 $('#total_amount_val').val(total);
+                            } else {
+                                old_intrest_amount = 0;
+                                intrest_on_difference = 0;
+                                total = 0;
+                                $('#ior_error').html('');
+                                $('#ida_error').html('Arrear charges not defined for selected year.');
+
+                                $('#oia').html(old_intrest_amount);
+                                $('#old_intrest_amount').val(old_intrest_amount);
+
+                                $('#dia').html(intrest_on_difference);
+                                $('#difference_intrest_amount').val(intrest_on_difference);
+                            
+                                $('#total_amount').html(total);
+                                $('#total_amount_val').val(total);
+                            }
+                        }
+                    });
+                    
+                // } else {
+                    // ior_month = ior_month - 1;
+                    // ida_month = ida_month - 1;
+                    // if(currentYear != ior_year || (ior_month <=3 && currentYear == ior_year )) {
+                    //     if(ior_month <=3 && currentYear == ior_year) {
+                    //         Year = currentYear -1;
+                    //     } else if(ida_month <=3 && currentYear == ida_year) {
+                    //         Year = ida_year;
+                    //     } else {
+                    //         Year = ior_year;
+                    //     }
+
+                    //     $.ajax({
+                    //         url:"{{URL::route('get_arrear_charges')}}",
+                    //         type: 'get',
+                    //         data: {year: Year,building_id:building_id,society_id:society_id },
+                    //             success: function(response){
+                    //             var strResponse = $.parseJSON(response);
+                                
+                    //             if (true == strResponse.result && null != strResponse.data) {
+                    //                 old_rate_old = Number(strResponse.data.old_rate);
+                    //                 old_rate_diff = Number(strResponse.data.revise_rate) - Number(strResponse.data.old_rate);
+                    //                 old_iod = Number(strResponse.data.interest_on_differance);
+                    //                 old_ior = Number(strResponse.data.interest_on_old_rate);
+                    //             } else {
+                    //                 $('#ior_error').html('');
+                    //                 $('#ida_error').html('Arrear charges not defined for selected year.');
+                    //             }
+                    //         }
+                    //     });
+                    // }
+
+                    // var months1 = monthDiff(
+                    //             new Date(ior_year, ior_month, 01),
+                    //             new Date(bill_year, bill_month, 01)  
+                    //          );
+                    //          // console.log(bill_year+' '+bill_month)
+                
+                    // var months2 = monthDiff(
+                    //                 new Date(ida_year, ida_month, 01),
+                    //                 new Date(bill_year, bill_month, 01)  
+                    //              );
+                    
+                    // iod_per = iod / 100;
+                    // ior_per = ior / 100;
+
+                    // if(ior_month > 3 && currentYear != ior_year || (ior_month <=3 && currentYear == ior_year)) {
+                    //     if(ior_month <=3 && currentYear == ior_year) {
+                    //         var old_monthDiff1 = monthDiff(
+                    //                 new Date(ior_year, ior_month, 01),
+                    //                 new Date(ior_year, 4, 01)
+                    //              );
+
+                    //         var old_monthDiff2 = monthDiff(
+                    //                 new Date(ida_year, ida_month, 01),
+                    //                 new Date(ida_year, 4, 01)
+                    //              );
+                    //     } else if (ior_month ==3 && currentYear == ior_year) {
+                    //         var old_monthDiff1 = 1;
+                    //         var old_monthDiff2 = 1;
+
+                    //     } else {
+
+                    //         var old_monthDiff1 = monthDiff(
+                    //                 new Date(ior_year, 3, 01),
+                    //                 new Date(ior_year, ior_month, 01)
+                    //              );
+
+                    //         var old_monthDiff2 = monthDiff(
+                    //                 new Date(ida_year, 3, 01),
+                    //                 new Date(ida_year, ida_month, 01)
+                    //              );
+                    //     }
+                    //     old_iod_per = old_iod / 100;
+                    //     old_ior_per = old_ior / 100;
+
+                    //     console.log(old_monthDiff1);
+                    //     console.log(old_monthDiff2);
+
+                    //     old_intrest_amount1 = (old_rate_old * old_ior_per * old_monthDiff1).toFixed(2);
+
+                    //     intrest_on_difference1 = (old_rate_diff * old_iod_per * old_monthDiff2).toFixed(2);
+
+                    //     var new_monthDiff1 = monthDiff(
+                    //                 new Date(bill_year, 3, 01),
+                    //                 new Date(bill_year, bill_month, 01)
+                    //              );
+
+                    //     var new_monthDiff2 = monthDiff(
+                    //                 new Date(bill_year, 3, 01),
+                    //                 new Date(bill_year, bill_month, 01)
+                    //              );
+
+                    //     old_intrest_amount2 = (old_rate * ior_per * new_monthDiff1).toFixed(2);
+
+                    //     intrest_on_difference2 = (rate_diff * iod_per * new_monthDiff2).toFixed(2);
+
+                    //     old_intrest_amount = (Number(old_intrest_amount1) + Number(old_intrest_amount2));
+
+                    //     intrest_on_difference = (Number(intrest_on_difference1) + Number(intrest_on_difference2));
+
+
+                    //     total1 = (parseFloat(old_rate_old *old_monthDiff1)+parseFloat(old_intrest_amount1)+parseFloat(old_rate_diff*old_monthDiff2)+parseFloat(intrest_on_difference1)).toFixed(2);
+
+                    //     total2 = (parseFloat(old_rate *new_monthDiff1)+parseFloat(old_intrest_amount2)+parseFloat(rate_diff*new_monthDiff2)+parseFloat(intrest_on_difference2)).toFixed(2);
+
+                    //     total = Number(total1) + Number(total2);
+                    // } else {
+
+                    //     // console.log(months2);
+                        
+                    //     // if(months1 > 0 ) {
+                    //         // var old_rate = old_rate *months1;
+                    //         // var rate_diff = rate_diff *months1;
+                    //     // }
+                    //     console.log(months1);
+                    //     old_intrest_amount = (old_rate * ior_per * months1).toFixed(2);
+
+                    //     intrest_on_difference = (rate_diff * iod_per * months2).toFixed(2);
+
+                    //     // $('#oia').html(old_intrest_amount);
+                    //     // $('#old_intrest_amount').val(old_intrest_amount);                
+
+                    //     // $('#dia').html(intrest_on_difference);
+                    //     // $('#difference_intrest_amount').val(intrest_on_difference);
+
+                    //     total = (parseFloat(old_rate *months1)+parseFloat(old_intrest_amount)+parseFloat(rate_diff*months1)+parseFloat(intrest_on_difference)).toFixed(2);
+                    // }
+                // }
+
+               
+
+                if(bill_year == '' || bill_month === ''){
+                    $('#bill_error').html('select Year and month for arrear Calculation.');
+                    return false;
+                } else if(ior_year == '' || ior_month === '') {
+                    $('#bill_error').html(''); 
+                    $('#ior_error').html('select Year and month of arrear Calculation.');
+                    return false;
+                } else if(ida_year == '' || ida_month === '') {
+                    $('#ior_error').html('');
+                    $('#ida_error').html('select Year and month of arrear Calculation.');
+                    return false;
+                } else {
+                    $('#ida_error').html('');
+                }
+                
+                
+                $('#oia').html(old_intrest_amount);
+                $('#old_intrest_amount').val(old_intrest_amount);
+
+                $('#dia').html(intrest_on_difference);
+                $('#difference_intrest_amount').val(intrest_on_difference);
+            
+                 $('#total_amount').html(total);
+                 $('#total_amount_val').val(total);
+
+    }
+
+function monthDiff(d1, d2) {
+    var months;
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months = months - d1.getMonth() + 1;
+    months = months + d2.getMonth() - 1;
+    return months <= 0 ? 0 : months;
+}
+
+
+function dateRange(startDate, endDate) {
+    // end_date = '2017-03-01';
+  var start      = startDate.split('-');
+  var end        = endDate.split('-');
+  var startYear  = parseInt(start[0]);
+  var endYear    = parseInt(end[0]);
+  var dates      = [];
+
+  for(var i = startYear; i <= endYear; i++) {
+    var endMonth = i != endYear ? 11 : parseInt(end[1]) - 1;
+    var startMon = i === startYear ? parseInt(start[1])-1 : 0;
+    for(var j = startMon; j <= endMonth; j = j > 12 ? j % 12 || 11 : j+1) {
+      var month = j+1;
+      var displayMonth = month < 10 ? '0'+month : month;
+      dates.push([i, displayMonth, '01'].join('-'));
+    }
+  }
+  return dates;
+}
+
+function formatDate(passedDate) {
+    var d = passedDate.getDate();
+    var m =  passedDate.getMonth();
+    var y = passedDate.getFullYear();
+
+    var returnDate = y+'-'+m+'-'+d;
+
+    return returnDate;
+}
 
 </script>
 @endsection
