806c9216d1f211533a150462d259ef1220d4d1d4
Sudesh J <sudesh.jadhav@neosofttech.com>
2019-01-25 11:53:18 +0530

    header changes

--- resources/views/admin/REE_department/offer_letter_header.blade.php
+++ resources/views/admin/REE_department/offer_letter_header.blade.php
@@ -4,7 +4,7 @@
             <h3 style="font-weight: bold;">मुंबई गृहनिर्माण व क्षेञविकास मंडळ</h3>
             <div style="margin-top: 0; font-weight: bold;">( म्हाडाचा घटक )</div>
             <div style="margin-top: 5px; text-transform: uppercase; font-weight: bold;">Mumbai Housing and <br> Area Development Board</div>
-            <div style="margin-top: 5px; font-weight: bold;">( A Mhada Unit )</div>
+            <div style="margin-top: 5px; font-weight: bold;">( A MHADA Unit )</div>
         </div>
         <div style="width: 50%; float: left;">
             <img src="{{ getcwd().'/img/logo-short.png' }}" style="width: 200px; float: right;"> 
