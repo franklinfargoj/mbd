8e1f7e364ab9257a29912398b038f8fc7eb52143
Sudesh J <sudesh.jadhav@neosofttech.com>
2019-03-13 16:15:13 +0530

    change in certificate

--- resources/views/admin/architect/certificate.blade.php
+++ resources/views/admin/architect/certificate.blade.php
@@ -9,7 +9,7 @@
         <div style="float: left; width: 63%;">&nbsp;</div>
         <div style="float: left; width: 37%;">
             <div style="text-align: left;">
-                <span>ARCH/M.B/ARCH. PANEL/312/2018</span>
+            <span>ARCH/M.B/ARCH. PANEL/{{$ArchitectApplication->id}}/{{date('Y')}}</span>
             </div>
             <div style="text-align: left;">
                 <span>Date:</span> {{date('d/m/Y')}}
@@ -20,7 +20,7 @@
     <h3 style="text-decoration: underline; text-align: center; font-size: 24px; margin-top: 40px; text-transform: uppercase;">Certificate</h3>
 
     <div style="margin-top: 30px; line-height: 1.8;">
-        <p style="text-indent: 25px; margin-top: 10px; margin-bottom: 10px;">We are pleased to inform you that your <span style="font-weight: bold;">firm M/s. {{$ArchitectApplication->name_of_applicant}}</span> is selected as Grade - <span style="font-weight: bold;"></span>
+        <p style="text-indent: 25px; margin-top: 10px; margin-bottom: 10px;">We are pleased to inform you that your <span style="font-weight: bold;">firm M/s. {{$ArchitectApplication->name_of_applicant}}</span> is selected as <span style="font-weight: bold;"></span>
         Architect /Consultant for the HOUSING panel of <span style="font-weight: bold;">Mumbai Housing and Area Development Board (MHADB)</span>, expiring on
         31/12/2022.</p>
         <p style="text-indent: 25px; margin-top: 10px; margin-bottom: 10px;">We expect your kind co-operation whenever any project is entrusted to you by MHADA during the above mentioned
