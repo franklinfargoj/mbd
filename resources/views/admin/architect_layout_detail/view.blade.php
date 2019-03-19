97da1b2c52e19c309d4a828b5a6207be188ed109
unknown <martin.philipose@wwindia.com>
2019-03-19 19:07:06 +0530

    design changes

--- resources/views/admin/architect_layout_detail/view.blade.php
+++ resources/views/admin/architect_layout_detail/view.blade.php
@@ -123,20 +123,17 @@
     </div>
 </div>
 <!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
+<div id="myModal" class="modal fade mhada-vld-modal" role="dialog">
     <div class="modal-dialog">
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title"></h4>
         </div>
-        <div class="modal-body">
+        <div class="modal-body p-0">
           <p id="project-id">Some text in the modal.</p>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
+        
       </div>
   
     </div>
