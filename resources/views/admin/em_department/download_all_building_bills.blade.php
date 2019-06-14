7f4830ea6bea795106afff25af1587861b332969
prajakta.sisale <prajakta.sisale@neosoftech.com>
2019-06-14 16:03:02 +0530

    worked on building bill pdfs

--- resources/views/admin/em_department/download_all_building_bills.blade.php
+++ resources/views/admin/em_department/download_all_building_bills.blade.php
@@ -20,11 +20,7 @@
         $total ='0';
 
         $tempBalance = 0;
-        // if($bill_data['lastBill']) {
-        //         if( 0 < $bill_data['lastBill']->total_bill_after_due_date ) {
-        //             $tempBalance += $bill_data['lastBill']->balance;
-        //         }
-        // }
+    
     @endphp
     @if(!$bill_data['arreasCalculation']->isEmpty())
         @foreach($bill_data['arreasCalculation'] as $calculation)
@@ -32,9 +28,7 @@
         @endforeach
     @endif
     <div>
-        <div>
-            <h3>Bill for {{date("M", strtotime("2001-" . $bill_data['month'] . "-01"))}}, {{$bill_data['year']}}</h3>
-        </div>
+            <h4>Bill for {{date("M", strtotime("2001-" . $bill_data['month'] . "-01"))}}, {{$bill_data['year']}}</h4>
         <table style="width: 720px;border-collapse: collapse;">
             <tr>
                 <td style="text-align:center;">
@@ -64,7 +58,7 @@
             </tr>
         </table>
 
-        <table style="width: 720px; border-collapse: collapse; margin-top: 30px;">
+        <table style="width: 720px; border-collapse: collapse; margin-top: 15px;">
             <tbody>
             <tr>
                 <td valign="top" style="background-color: #FFEFD5; padding: 5px;">
@@ -156,8 +150,8 @@
             </tr>
             </tbody>
         </table>
-        <div style="border: 1px solid #000; padding: 5px; margin-top: 30px;background-color: lightblue;"><h3 style="text-align: center;">Bill Summary - {{date("M", strtotime("2001-" . $bill_data['month'] . "-01"))}}, {{date('Y',strtotime($bill_data['TransBillGenerate']->bill_date))}}</h3></div>
-        <table style="width: 100%; border-collapse: collapse; margin-top: 30px;">
+        {{--<div style="border: 1px solid #000; padding: 5px; margin-top: 30px;background-color: lightblue;"><h3 style="text-align: center;">Bill Summary - {{date("M", strtotime("2001-" . $bill_data['month'] . "-01"))}}, {{date('Y',strtotime($bill_data['TransBillGenerate']->bill_date))}}</h3></div>--}}
+        <table style="width: 100%; border-collapse: collapse; margin-top: 15px;">
             <thead>
             <tr>
                 <th valign="top" style="background-color: lightblue; padding:10px 5px; width: 40%;">Bill Title - {{date("M", strtotime("2001-" . $bill_data['month'] . "-01"))}}</th>
@@ -219,8 +213,11 @@
             @if(!$bill_data['arreasCalculation']->isEmpty())
                 @php $total ='0'; @endphp
 
-                <div style="border: 1px solid #000; padding: 5px; margin-top: 160px;"><h3 style="text-align: center;">Balance amount to be paid - Arrears</h3></div>
+                {{--<div style="border: 1px solid #000; padding: 5px; margin-top: 160px;"><h3 style="text-align: center;">Balance amount to be paid - Arrears</h3></div>--}}
                 <table class="display table table-responsive table-bordered" style="width:100%">
+                    <tr>
+                        <th valign="top" style=" padding: 5px;background-color:  #94b8c4;" colspan="4">Balance amount to be paid - Arrears</th>
+                    </tr>
                     <tr>
                         <th valign="top" style="border: 1px solid #000; padding: 5px; width: 25%;">Year</th>
                         <th valign="top" style="border: 1px solid #000; padding: 5px; width: 25%;">Month</th>
@@ -245,15 +242,7 @@
                             $calculation_month=$calculation->month;
                             $calculation_year=$calculation->year;
                         @endphp
-                        {{-- <tr>
-                            <td>{{$calculation->year}} <input name='arrear_id[]' type='text' value='{{$calculation->id}}'
-                                    hidden> </td>
-                            <td>{{date("M", strtotime("2001-" . $calculation->month . "-01"))}}</td>
-                            <td>{{$calculation->total_amount - $calculation->old_intrest_amount -
-                                $calculation->difference_intrest_amount }}</td>
-                            <td>{{$calculation->old_intrest_amount +
-                                $calculation->difference_intrest_amount}}</td>
-                        </tr> --}}
+
                     @endforeach
                     <tr>
                         <td valign="top" style="border: 1px solid #000; padding: 5px; text-align: center;">{{$calculation_year}} </td>
@@ -269,9 +258,12 @@
                 </table>
             @endif
         @else
-            <div style="border: 2px solid #000; padding: 5px; margin-top: 20px;"><h3 style="text-align: center;">Balance amount to be paid - Arrears</h3></div>
-            <table style="width: 100%; border-collapse: collapse; margin-top: 30px;">
+            {{--<div style="border: 2px solid #000; padding: 5px; margin-top: 20px;"><h3 style="text-align: center;">Balance amount to be paid - Arrears</h3></div>--}}
+            <table style="width: 100%; border-collapse: collapse; margin-top: 15px;">
                 <thead>
+                <tr>
+                    <th valign="top" style="padding: 5px;background-color:  #94b8c4;" colspan="4">Balance amount to be paid - Arrears</th>
+                </tr>
                 <tr>
                     <th valign="top" style="border: 1px solid #000; padding: 5px; width: 25%;">Year</th>
                     <th valign="top" style="border: 1px solid #000; padding: 5px; width: 25%;">Month</th>
@@ -299,9 +291,12 @@
                 </tbody>
             </table>
         @endif
-        <div style="border: 2px solid #000; padding: 5px; margin-top: 30px;"><h3 style="text-align: center;">Total amount to be paid</h3></div>
-        <table style="width: 100%; border-collapse: collapse; margin-top: 30px;">
+        {{--<div style="border: 2px solid #000; padding: 5px; margin-top: 30px;"><h3 style="text-align: center;">Total amount to be paid</h3></div>--}}
+        <table style="width: 100%; border-collapse: collapse; margin-top: 15px;">
             <thead>
+            <tr>
+                <th valign="top" style="padding: 5px;background-color:  #94b8c4;" colspan="2">Total amount to be paid</th>
+            </tr>
             <tr>
                 <th valign="top" style=" padding: 5px; width: 50%; background-color: lightblue;">Particulars</th>
                 <th valign="top" style=" padding: 5px; width: 50%; background-color: lightblue;">Amount in Rs.</th>
@@ -340,19 +335,7 @@
                 <td valign="top" style="background-color: #f1f3f4;  padding: 5px;">Bill Amount After due date</td>
                 <td valign="top" style="background-color: #f1f3f4;  padding: 5px; text-align: center;" class="text-center">{{ceil($bill_data['TransBillGenerate']->total_service_after_due+$bill_data['TransBillGenerate']->prev_service_charge_balance+$bill_data['TransBillGenerate']->prev_arrear_balance+$bill_data['TransBillGenerate']->prev_arrear_interest_balance-$bill_data['TransBillGenerate']->prev_credit)}}</td>
             </tr>
-            {{-- <tr>
-                <td valign="top" style="background-color: #f1f3f4;  padding: 5px; font-weight: bold;">Grand Total</td>
-                <td valign="top" style="background-color: #f1f3f4;  padding: 5px; text-align: center; font-weight: bold;">{{$bill_data['TransBillGenerate']->total_bill_temp}}</td>
-            </tr> --}}
             </tbody>
         </table>
     </div>
-    </div>
-    {{--<hr/>--}}
-    {{--<pagebreak />--}}
-
-{{--@endforeach--}}
-
-
-
 </body>
